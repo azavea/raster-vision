4c3aca6dc6237f147bd384f93807e646c05d6f6c
Lewis Fishgold <lewfish@gmail.com>
2017-06-27 13:55:27 -0400

    Add agg_concat job to combine tag subset models

---
+++ src/experiments/tagging/tests/agg_concat_test/generate_experiments.py
@@ -0,0 +1,58 @@
+from os.path import join
+from copy import deepcopy
+
+from rastervision.experiment_generator import (
+    ExperimentGenerator, get_parent_dir)
+from rastervision.tagging.data.planet_kaggle import Dataset
+
+
+class TestExperimentGenerator(ExperimentGenerator):
+    def generate_experiments(self):
+        base_exp = {
+            'batch_size': 1,
+            'problem_type': 'tagging',
+            'dataset_name': 'planet_kaggle',
+            'generator_name': 'jpg',
+            'active_input_inds': [0, 1, 2],
+            'use_pretraining': True,
+            'optimizer': 'adam',
+            'init_lr': 1e-3,
+            'model_type': 'baseline_resnet',
+            'train_ratio': 0.8,
+            'epochs': 2,
+            'nb_eval_samples': 10,
+            'nb_eval_plot_samples': 3,
+            'validation_steps': 1,
+            'run_name': 'tagging/tests/agg_concat_test',
+            'steps_per_epoch': 2,
+            'augment_methods': ['hflip', 'vflip', 'rotate', 'translate'],
+            'rare_sample_prob': 0.5
+        }
+
+        dataset = Dataset()
+        active_tags_list = [
+            dataset.atmos_tags, dataset.rare_tags, dataset.common_tags]
+
+        exps = []
+        exp_count = 0
+        for active_tags in active_tags_list:
+            exp = deepcopy(base_exp)
+            exp['active_tags'] = active_tags
+            exp['run_name'] = join(exp['run_name'], str(exp_count))
+            exps.append(exp)
+            exp_count += 1
+
+        agg_exp = deepcopy(base_exp)
+        agg_exp['run_name'] = join(base_exp['run_name'], str(exp_count))
+        agg_exp['aggregate_run_names'] = [exp['run_name'] for exp in exps]
+        agg_exp['aggregate_type'] = 'agg_concat'
+        agg_exp['active_tags'] = dataset.all_tags
+        exps.append(agg_exp)
+        exp_count += 1
+
+        return exps
+
+
+if __name__ == '__main__':
+    path = get_parent_dir(__file__)
+    gen = TestExperimentGenerator().run(path)
--- src/rastervision/tagging/tasks/predict.py
+++ src/rastervision/tagging/tasks/predict.py
@@ -1,8 +1,10 @@
 from os.path import join
+import json
 
 import numpy as np
 
 from rastervision.common.settings import results_path
+from rastervision.common.options import AGG_CONCAT, AGG_ENSEMBLE
 
 from rastervision.tagging.data.planet_kaggle import TagStore
 from rastervision.tagging.tasks.utils import compute_prediction
@@ -17,13 +19,53 @@ VALIDATION_PREDICT = 'validation_predict'
 TEST_PREDICT = 'test_predict'
 
 
+def get_probs_fn(split):
+    return '{}_probs.npy'.format(split)
+
+
+def get_preds_fn(split):
+    return '{}_preds.csv'.format(split)
+
+
+def compute_concat_probs(run_path, options, generator, split):
+    probs_path = join(run_path, get_probs_fn(split))
+
+    y_probs_list = []
+    active_tags_list = []
+
+    for run_name in options.aggregate_run_names:
+        run_probs_path = join(
+            results_path, run_name, get_probs_fn(split))
+        options_path = join(
+            results_path, run_name, 'options.json')
+
+        with open(options_path) as options_file:
+            # TODO check that active_tags are disjoint
+            options_dict = json.load(options_file)
+            active_tags = options_dict.get(
+                'active_tags', generator.dataset.all_tags)
+            active_tags_list.append(active_tags)
+            y_probs_list.append(np.load(run_probs_path))
+
+    nb_samples = y_probs_list[0].shape[0]
+    nb_active_tags = sum(map(lambda x: len(x), active_tags_list))
+    concat_y_probs = np.zeros((nb_samples, nb_active_tags))
+    print(concat_y_probs.shape)
+    for y_probs, active_tags in zip(y_probs_list, active_tags_list):
+        for tag_ind, active_tag in enumerate(active_tags):
+            concat_tag_ind = generator.tag_store.get_tag_ind(active_tag)
+            concat_y_probs[:, concat_tag_ind] = y_probs[:, tag_ind]
+
+    np.save(probs_path, concat_y_probs)
+
+
 def compute_ensemble_probs(run_path, options, generator, split):
-    probs_path = join(run_path, '{}_probs.npy'.format(split))
+    probs_path = join(run_path, get_probs_fn(split))
     y_probs = []
 
     for run_name in options.aggregate_run_names:
         run_probs_path = join(
-            results_path, run_name, '{}_probs.npy'.format(split))
+            results_path, run_name, get_probs_fn(split))
         y_probs.append(np.expand_dims(np.load(run_probs_path), axis=2))
 
     y_probs = np.concatenate(y_probs, axis=2)
@@ -36,7 +78,7 @@ def compute_ensemble_probs(run_path, options, generator, split):
 
 
 def compute_probs(run_path, model, options, generator, split):
-    probs_path = join(run_path, '{}_probs.npy'.format(split))
+    probs_path = join(run_path, get_probs_fn(split))
     y_probs = []
 
     batch_size = options.batch_size
@@ -60,10 +102,10 @@ def compute_probs(run_path, model, options, generator, split):
 
 
 def compute_preds(run_path, options, generator, split):
-    probs_path = join(run_path, '{}_probs.npy'.format(split))
+    probs_path = join(run_path, get_probs_fn(split))
     y_probs = np.load(probs_path)
 
-    predictions_path = join(run_path, '{}_preds.csv'.format(split))
+    predictions_path = join(run_path, get_preds_fn(split))
     thresholds = load_thresholds(run_path)
     tag_store = TagStore(active_tags=options.active_tags)
     file_inds = generator.get_file_inds(split)
