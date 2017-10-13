bea7142dac0f64fc8b8667f4a8f62c99e7a023d9
Lewis Fishgold <lewfish@gmail.com>
2017-10-13 17:29:00 -0400

    Add script to infer label map
    This reads a bunch of GeoJSON annotation files,
    infers the label map, and writes it to a protobuf file.

---
+++ src/detection/rv/commands/make_label_map.py
@@ -0,0 +1,55 @@
+import json
+
+import click
+
+
+def get_labels(label_paths):
+    labels = set()
+    for label_path in label_paths:
+        with open(label_path, 'r') as label_file:
+            geojson = json.load(label_file)
+            features = geojson['features']
+
+            for feature in features:
+                if 'properties' in feature:
+                    if 'label' in feature['properties']:
+                        labels.add(feature['properties']['label'])
+                    else:
+                        labels.add('Unknown')
+
+    labels = sorted(list(labels))
+    return labels
+
+
+def write_label_map(path, labels):
+    with open(path, 'w') as label_map_file:
+        for class_id, label in enumerate(labels, start=1):
+            item_str = '''
+item {{
+  id: {}
+  name: \'{}\'
+}}
+'''.format(class_id, label)
+            label_map_file.write(item_str)
+
+
+def _make_label_map(label_paths, label_map_path):
+    labels = get_labels(label_paths)
+    write_label_map(label_map_path, labels)
+
+
+@click.command()
+@click.argument('label_paths', nargs=-1)
+@click.argument('label_map_path')
+def make_label_map(label_paths, label_map_path):
+    """Generate a label map based on the labels in a set of label files.
+
+    Args:
+        label_paths: List of paths to GeoJSON files containing labels
+        label_map_path: Path to Protobuf file with map of labels to ids
+    """
+    _make_label_map(label_paths, label_map_path)
+
+
+if __name__ == '__main__':
+    make_label_map()
