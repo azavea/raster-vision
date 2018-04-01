34a2914723619e86621a561b9c1127a4e067736b
Lewis Fishgold <lewfish@gmail.com>
2018-04-01 14:55:36 -0400

    Exclude chips from areas outside the footprint

--- src/rastervision/ml_tasks/classification.py
+++ src/rastervision/ml_tasks/classification.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from rastervision.core.ml_task import MLTask
 from rastervision.evaluations.classification_evaluation import (
     ClassificationEvaluation)
@@ -8,7 +10,12 @@ class Classification(MLTask):
         extent = project.raster_source.get_extent()
         chip_size = options.chip_size
         stride = chip_size
-        return extent.get_windows(chip_size, stride)
+        windows = []
+        for window in extent.get_windows(chip_size, stride):
+            chip = project.raster_source.get_chip(window)
+            if np.sum(chip.ravel()) > 0:
+                windows.append(window)
+        return windows
 
     def get_train_labels(self, window, project, options):
         return project.ground_truth_label_store.get_labels(window)
