5a6ee2f7099179e25db3746c63070cdcbe2c6783
Lewis Fishgold <lewfish@gmail.com>
2018-07-03 13:50:04 -0400

    Clarify crs_transformer naming
    Change web to map in crs_transformer method and variable names and add
    more comments. This is to reflect the fact that map-based coordinates are not
    in web mercator.

--- src/rastervision/crs_transformers/rasterio_crs_transformer.py
+++ src/rastervision/crs_transformers/rasterio_crs_transformer.py
@@ -4,22 +4,48 @@ from rastervision.core.crs_transformer import CRSTransformer
 
 
 class RasterioCRSTransformer(CRSTransformer):
+    """Transformer for a RasterioRasterSource.
+
+    This assumes that the map coordinates are always in lon/lat format.
+    """
+
     def __init__(self, image_dataset):
+        """Construct transformer.
+
+        Args:
+            image_dataset: Rasterio DatasetReader
+        """
         self.image_dataset = image_dataset
-        self.web_proj = pyproj.Proj(init='epsg:4326')
+        self.map_proj = pyproj.Proj(init='epsg:4326')
         image_crs = image_dataset.crs['init']
         self.image_proj = pyproj.Proj(init=image_crs)
 
-    def web_to_pixel(self, web_point):
+    def map_to_pixel(self, map_point):
+        """Transform point from map to pixel-based coordinates.
+
+        Args:
+            map_point: (long, lat) tuple
+
+        Returns:
+            (x, y) tuple in pixel coordinates
+        """
         image_point = pyproj.transform(
-            self.web_proj, self.image_proj, web_point[0], web_point[1])
+            self.map_proj, self.image_proj, map_point[0], map_point[1])
         pixel_point = self.image_dataset.index(image_point[0], image_point[1])
         pixel_point = (pixel_point[1], pixel_point[0])
         return pixel_point
 
-    def pixel_to_web(self, pixel_point):
+    def pixel_to_map(self, pixel_point):
+        """Transform point from pixel to map-based coordinates.
+
+        Args:
+            pixel_point: (x, y) tuple in pixel coordinates
+
+        Returns:
+            (lon, lat) tuple
+        """
         image_point = self.image_dataset.ul(
             int(pixel_point[1]), int(pixel_point[0]))
-        web_point = pyproj.transform(
-            self.image_proj, self.web_proj, image_point[0], image_point[1])
-        return web_point
+        map_point = pyproj.transform(
+            self.image_proj, self.map_proj, image_point[0], image_point[1])
+        return map_point
