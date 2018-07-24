d3cbf5e395f3c6e38d7b4c729b22e4fbdb76ed35
James McClain <jmcclain@azavea.com>
2018-07-24 12:56:45 -0400

    Source-to-Destination Label Translation

--- src/rastervision/ml_tasks/semantic_segmentation.py
+++ src/rastervision/ml_tasks/semantic_segmentation.py
@@ -1,15 +1,12 @@
 import numpy as np
 
 from rastervision.core.ml_task import MLTask
-from rastervision.core.box import Box
 
 
 class SemanticSegmentation(MLTask):
     def get_train_windows(self, scene, options):
         raster_source = scene.raster_source
-        meta = raster_source.image_dataset.meta
-        width = meta.get('width')
-        height = meta.get('height')
+        extent = raster_source.get_extent()
 
         # label_store = scene.ground_truth_label_store
 
@@ -17,15 +14,18 @@ class SemanticSegmentation(MLTask):
 
         windows = []
         for i in range(100):  # XXX insensitive
-            xmin = np.random.randint(low=0, high=width - chip_size)
-            ymin = np.random.randint(low=0, high=height - chip_size)
-            windows.append(Box.make_square(ymin, xmin, chip_size))
+            windows.append(extent.make_random_square(chip_size))
 
         return windows
 
     def get_train_labels(self, window, scene, options):
-        chip = scene.ground_truth_label_store.src.get_chip(window)
+        label_store = scene.ground_truth_label_store
+        chip = label_store.src._get_chip(window)
+        fn = label_store.fn
+
         bit2 = (chip[:, :, 0] > 0)
         bit1 = (chip[:, :, 1] > 0)
         bit0 = (chip[:, :, 2] > 0)
-        return np.array(bit2 * 4 + bit1 * 2 + bit0 * 1, dtype=np.uint8)
+        retval = np.array(bit2 * 4 + bit1 * 2 + bit0 * 1, dtype=np.uint8)
+
+        return np.array(fn(retval), dtype=np.uint8)
