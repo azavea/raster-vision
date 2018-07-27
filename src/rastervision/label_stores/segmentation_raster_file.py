0f82fce0690d245c8dd3ecb716b35681c56c7882
James McClain <jmcclain@azavea.com>
2018-07-27 11:36:02 -0400

    Label Store Unit Tests

--- src/rastervision/label_stores/segmentation_raster_file.py
+++ src/rastervision/label_stores/segmentation_raster_file.py
@@ -1,15 +1,27 @@
 import numpy as np
 
+from rastervision.core.box import Box
 from rastervision.core.label_store import LabelStore
+from rastervision.core.raster_source import RasterSource
 from rastervision.builders import raster_source_builder
+from rastervision.protos.raster_source_pb2 import (RasterSource as
+                                                   RasterSourceProto)
 
 
 class SegmentationRasterFile(LabelStore):
     def __init__(self, src, dst, src_classes=[], dst_classes=[]):
-        if src is not None:
-            self.src = raster_source_builder.build(src)
+        self.set_labels(src)
+
+        if isinstance(dst, RasterSource):
+            self.dst = dst
+        elif isinstance(dst, RasterSourceProto):
+            self.dst = raster_source_builder.build(dst)
+        elif dst is None or dst is '':
+            self.dst = None
+        elif isinstance(dst, str):
+            pass  # XXX str instead of RasterSourceProto
         else:
-            self.src = None
+            raise ValueError('Unsure how to handle dst={}'.format(type(dst)))
 
         correspondence = dict(zip(src_classes, dst_classes))
 
@@ -23,13 +35,33 @@ class SegmentationRasterFile(LabelStore):
         self.correspondence = correspondence
 
     def clear(self):
-        pass
+        self.src = None
 
-    def set_labels(self, labels):
-        pass
+    def set_labels(self, src):
+        if isinstance(src, RasterSource):
+            self.src = src
+        elif isinstance(src, RasterSourceProto):
+            self.src = raster_source_builder.build(src)
+        elif src is None:
+            self.src = None
+        else:
+            raise ValueError('Unsure how to handle src={}'.format(type(src)))
+
+        if self.src is not None:
+            small_box = Box(0, 0, 1, 1)
+            (self.channels, _, _) = self.src._get_chip(small_box).shape
+        else:
+            self.channels = 1
 
     def get_labels(self, window=None):
-        pass
+        if self.src is not None:
+            return self.src._get_chip(window)
+        else:
+            ymin = window.ymin
+            xmin = window.xmin
+            ymax = window.ymax
+            xmax = window.xmax
+            return np.zeros((self.channels, ymax - ymin, xmax - xmin))
 
     def extend(self, labels):
         pass
