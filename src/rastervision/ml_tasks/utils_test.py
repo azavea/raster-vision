1f559f9e1dadd071bd54bcd583637f69b13c56d4
Simon Kassel <skassel@azavea.com>
2018-08-16 14:29:25 -0400

    style changes

--- src/rastervision/ml_tasks/utils_test.py
+++ src/rastervision/ml_tasks/utils_test.py
@@ -1,31 +1,13 @@
 import unittest
-import tempfile
-import os
-import json
 
 from rastervision.core.box import Box
-from rastervision.core.crs_transformer import CRSTransformer
-from rastervision.label_stores.utils import json_to_shapely
 from rastervision.ml_tasks.utils import is_window_inside_aoi
-
-
-class DoubleCRSTransformer(CRSTransformer):
-    """Mock CRSTransformer used for testing.
-
-    Assumes map coords are 2x pixels coords.
-    """
-
-    def map_to_pixel(self, web_point):
-        return (web_point[0] * 2, web_point[1] * 2)
-
-    def pixel_to_map(self, pixel_point):
-        return (pixel_point[0] / 2, pixel_point[1] / 2)
 
 
 class TestMLTaskUtils(unittest.TestCase):
     def setUp(self):
         self.aoi_polygon = [Box(0, 0, 5, 5).get_shapely()]
-        
+
     def tearDown(self):
         pass
 
