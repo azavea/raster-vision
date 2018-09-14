c65fd28df680f5d0e463b540be171d170f46b18f
lossyrob <rdemanuele@gmail.com>
2018-09-14 19:41:01 -0400

    Move class map functionality, unit test.

--- src/rastervision/core/class_map.py
+++ src/rastervision/core/class_map.py
@@ -1,3 +1,8 @@
+from typing import (List, Dict, Tuple, Union)
+
+from rastervision.protos.class_item_pb2 \
+    import ClassItem as ClassItemMsg
+
 class ClassItem(object):
     """A class id and associated data."""
 
@@ -26,6 +31,17 @@ class ClassItem(object):
         if self.color:
             s += ' ({})'.format(self.color)
         return s
+
+    def to_proto(self):
+        return ClassItemMsg(id=self.id,
+                            name=self.name,
+                            color=self.color)
+
+    @staticmethod
+    def from_proto(msg):
+        return ClassItem(id=msg.id,
+                         name=msg.name,
+                         color=msg.color)
 
 
 class ClassMap(object):
@@ -86,3 +102,60 @@ class ClassMap(object):
                 'name': class_item.name
             }
         return category_index
+
+    def to_proto(self):
+        """Transform a ClassMap into
+        a list of ClassItem protobuf messages
+        """
+        return [
+            item.to_proto()
+            for item in self.get_items()
+        ]
+
+    @staticmethod
+    def construct_from(classes):
+        """Construct ClassMap from a number of different
+           representations.
+
+            Args:
+                classes: One of the following:
+                         - a ClassMap
+                         - a list of class names
+                         - a list of ClassItem protobuf messages
+                         - a list of ClassItems
+                         - a dict which maps class names to class ids
+                         - a dict which maps class names to a tuple of
+                           (class_id, color), where color is a PIL color string.
+        """
+        result = None
+        if type(classes) is ClassMap:
+            result = classes
+        elif type(classes) is dict:
+            item_list = []
+            if not len(classes.items()) == 0:
+                if type(list(classes.items())[0][1]) is tuple:
+                    # This dict already has colors mapped to class ids
+                    for name, (class_id, color) in classes.items():
+                        item_list.append(ClassItem(class_id, name, color))
+                else:
+                    # Map items to empty colors
+                    for name, class_id in classes.items():
+                        item_list.append(ClassItem(class_id, name))
+            result = ClassMap(item_list)
+        elif type(classes) is list:
+            item_list = []
+            if not len(classes) == 0:
+                if type(classes[0]) is ClassItemMsg:
+                    for item in classes:
+                        item_list.append(ClassItem(item.id, item.name, item.color))
+                elif type(classes[0]) is str:
+                    for i, name in enumerate(classes):
+                        item_list.append(ClassItem(i + 1, name))
+                else:
+                    item_list = classes
+            result = ClassMap(item_list)
+        else:
+            raise Exception('Cannot convert type {} to ClassMap'.format(
+                type(classes)))
+
+        return result
