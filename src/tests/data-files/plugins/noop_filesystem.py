314f83e1fec490e1bd6d8b24c10eb844eb0f4f51
lossyrob <rdemanuele@gmail.com>
2018-09-17 20:33:17 -0400

    Add noop-filesystem plugin for tests.

---
+++ src/tests/data-files/plugins/noop_filesystem.py
@@ -0,0 +1,48 @@
+from rastervision.filesystem import FileSystem
+
+
+class NoopFileSystem(FileSystem):
+    @staticmethod
+    def matches_uri(uri: str) -> bool:
+        True
+
+    @staticmethod
+    def file_exists(uri: str) -> bool:
+        False
+
+    @staticmethod
+    def read_str(uri: str) -> str:
+        return ''
+
+    @staticmethod
+    def read_bytes(uri: str) -> bytes:
+        return None
+
+    @staticmethod
+    def write_str(uri: str, data: str) -> None:
+        pass
+
+    @staticmethod
+    def write_bytes(uri: str, data: bytes) -> None:
+        pass
+
+    @staticmethod
+    def sync_dir(src_dir_uri: str, dest_dir_uri: str,
+                 delete: bool = False) -> None:
+        pass
+
+    @staticmethod
+    def copy_to(src_path: str, dst_uri: str) -> None:
+        pass
+
+    @staticmethod
+    def copy_from(uri: str, path: str) -> None:
+        pass
+
+    @staticmethod
+    def local_path(uri: str, download_dir: str) -> None:
+        pass
+
+
+def register_plugin(plugin_registry):
+    plugin_registry.register_filesysystem(NoopFileSystem)
