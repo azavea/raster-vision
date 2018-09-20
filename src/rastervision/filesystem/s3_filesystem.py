abc8d4b4289dad5535c56a878e9459eeda5d63f5
lossyrob <rdemanuele@gmail.com>
2018-09-20 17:11:43 -0400

    Rewrote how we check file existing in s3 to be faster

--- src/rastervision/filesystem/s3_filesystem.py
+++ src/rastervision/filesystem/s3_filesystem.py
@@ -17,16 +17,17 @@ class S3FileSystem(FileSystem):
     def file_exists(uri: str) -> bool:
         # Lazily load boto
         import boto3
+        import botocore
 
         s3 = boto3.resource('s3')
         parsed_uri = urlparse(uri)
-        bucket = s3.Bucket(parsed_uri.netloc)
+        bucket = parsed_uri.netloc
         key = parsed_uri.path[1:]
-        objs = list(bucket.objects.filter(Prefix=key))
-        if len(objs) > 0 and objs[0].key == key:
-            return True
-        else:
+        try:
+            s3.Object(bucket, key).load()
+        except botocore.exceptions.ClientError as e:
             return False
+        return True
 
     @staticmethod
     def read_str(uri: str) -> str:
