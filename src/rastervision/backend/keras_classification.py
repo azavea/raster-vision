d95d4e0b09f7121587bb8fe6bd3ffcc91141b928
lossyrob <rdemanuele@gmail.com>
2018-09-24 14:33:12 -0400

    If we are replacing the model in keras, don't sync down previous run.

--- src/rastervision/backend/keras_classification.py
+++ src/rastervision/backend/keras_classification.py
@@ -224,7 +224,8 @@ class KerasClassification(Backend):
         backend_config_path, pretrained_model_path = model_paths
 
         # Get output from potential previous run so we can resume training.
-        sync_from_dir(self.config.training_output_uri, model_files.base_dir)
+        if not self.config.train_options.replace_model:
+            sync_from_dir(self.config.training_output_uri, model_files.base_dir)
 
         sync = start_sync(
             model_files.base_dir,
