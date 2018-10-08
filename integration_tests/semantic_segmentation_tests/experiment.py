8b0f48f5bd237587f0bbdfd63d250d8dd2c3c4e7
Lewis Fishgold <lewfish@gmail.com>
2018-10-07 21:27:45 -0400

    Add working segmentation integration test

--- integration_tests/semantic_segmentation_tests/experiment.py
+++ integration_tests/semantic_segmentation_tests/experiment.py
@@ -8,41 +8,55 @@ class SemanticSegmentationIntegrationTest(rv.ExperimentSet):
         def get_path(part):
             return os.path.join(os.path.dirname(__file__), part)
 
-        # img_path = get_path('scene/image.tif')
-        # label_path = get_path('scene/labels.tif')
+        img_path = get_path('scene/image.tif')
+        label_path = get_path('scene/labels.tif')
+        class_map = {'red': (1, 'red'), 'green': (2, 'green')}
+        num_steps = 1
+        batch_size = 1
 
-        img_path = ('s3://raster-vision-rob-dev/integration-tests/'
-                    'semantic_segmentation_tests/scene/image.tif')
-        label_path = ('s3://raster-vision-rob-dev/integration-tests/'
-                      'semantic_segmentation_tests/scene/labels.tif')
+        # These are the parameters that were used to train the following pretrained
+        # model.
+        # num_steps = 5000
+        # batch_size = 8
+        # I found it was also possible to train for 2000 steps with the same eval,
+        # and even fewer steps may be possible. But because it takes
+        # 5 secs/step with batch size of 1 on a CPU, it doesn't seem feasible to actually
+        # train during CI. So instead we just use the model trained on the GPU and then
+        # fine-tune it for one step.
+        pretrained_model = (
+            'https://github.com/azavea/raster-vision-data/releases/'
+            'download/0.0.6/deeplab-test-model.tar.gz')
 
-        class_map = {'car': (1, 'red'), 'building': (2, 'green')}
-
+        # This a divisor of the scene length.
+        chip_size = 300
         task = rv.TaskConfig.builder(rv.SEMANTIC_SEGMENTATION) \
-                            .with_chip_size(256) \
+                            .with_chip_size(chip_size) \
                             .with_classes(class_map) \
                             .with_chip_options(window_method='sliding',
-                                               stride=256) \
+                                               stride=chip_size,
+                                               debug_chip_probability=1.0) \
                             .build()
 
+        # .with_config belowe needed to copy final layer from pretrained model.
         backend = rv.BackendConfig.builder(rv.TF_DEEPLAB) \
                                   .with_task(task) \
                                   .with_model_defaults(rv.MOBILENET_V2) \
+                                  .with_pretrained_model(pretrained_model) \
                                   .with_train_options(do_monitoring=True,
                                                       replace_model=True) \
-                                  .with_num_steps(30000) \
-                                  .with_batch_size(8) \
+                                  .with_num_steps(num_steps) \
+                                  .with_batch_size(batch_size) \
                                   .with_debug(True) \
-                                  .with_config({'saveIntervalSecs': 5}) \
-                                  .with_config({'saveSummariesSecs': 5}) \
+                                  .with_config({'initializeLastLayer': 'true'}) \
                                   .build()
 
         label_source = rv.LabelSourceConfig.builder(rv.SEMANTIC_SEGMENTATION_RASTER) \
-                                           .with_source_class_map(task.class_map) \
+                                           .with_rgb_class_map(task.class_map) \
                                            .with_raster_source(label_path) \
                                            .build()
 
         label_store = rv.LabelStoreConfig.builder(rv.SEMANTIC_SEGMENTATION_RASTER) \
+                                         .with_rgb(True) \
                                          .build()
 
         scene = rv.SceneConfig.builder() \
