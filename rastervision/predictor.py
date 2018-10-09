23641d747fc0d6c78ad90ba606af99579300ca10
lossyrob <rdemanuele@gmail.com>
2018-10-08 22:39:43 -0400

    Add docstrings for predictor and plugin registry

--- rastervision/predictor.py
+++ rastervision/predictor.py
@@ -8,13 +8,31 @@ from rastervision.protos.command_pb2 import CommandConfig as CommandConfigMsg
 
 
 class Predictor():
-    """Class for making predictions based off of a prediction package"""
+    """Class for making predictions based off of a prediction package."""
 
     def __init__(self,
                  prediction_package_uri,
                  tmp_dir,
                  update_stats=False,
                  channel_order=None):
+        """Creates a new Predictor.
+
+        Args:
+          prediction_packaage_uri - The URI of the prediction package to use.
+                                    Can be any type of URI that Raster Vision can read.
+          tmp_dir - Temporary directory in which to store files that are used by the
+                    Predictor. This directory is not cleaned up by this class.
+          update_stats - Option indicating if any Analyzers should be run on the image
+                         to be predicted on. Otherwise, the Predictor will use the output
+                         of Analyzers that are bundled with the predict package. This is
+                         useful, for instance, if you are predicting against imagery that
+                         needs to be normalized with a StatsAnalyzer, and the color profile
+                         of the new imagery is significantly different then the imagery
+                         the model was trained on.
+          channel_order - Option indicating a new channel order to use for the imagery being
+                          predicted against. If not present, the channel_order from the original
+                          configuration in the predict package will be used.
+        """
         self.tmp_dir = tmp_dir
         self.update_stats = update_stats
         self.model_loaded = False
@@ -73,6 +91,14 @@ class Predictor():
                                        .build()
 
     def load_model(self):
+        """Load the model for this Predictor.
+
+        This is useful if you are going to make multiple predictions with the model,
+        and want it to be fast on the first prediction.
+
+        Note: This is called implicitly on the first call of 'predict' if it hasn't
+              been called already.
+        """
         self.backend = self.backend_config.create_backend(self.task_config)
         self.backend.load_model(self.tmp_dir)
         self.task = self.task_config.create_task(self.backend)
@@ -82,6 +108,20 @@ class Predictor():
         self.model_loaded = True
 
     def predict(self, image_uri, label_uri=None, config_uri=None):
+        """Generate predictions for the given image.
+
+        Args:
+           image_uri - URI of the image to make predictions against.
+                       This can be any type of URI readable by Raster Vision
+                       FileSystems.
+           label_uri - Optional URI to save labels  off into.
+           config_uri - Optional URI in which to save the bundle_config,
+                        which can be useful to client applications for understanding
+                        how to interpret the labels.
+
+           Returns:
+              rastervision.data.labels.Labels containing the predicted labels.
+        """
         if not self.model_loaded:
             self.load_model()
         scene_config = self.scene_config.for_prediction(image_uri, label_uri) \
