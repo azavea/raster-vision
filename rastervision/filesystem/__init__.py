3ca9747ffbf53c3e4e86865dbab62a24551a8448
lossyrob <rdemanuele@gmail.com>
2018-10-09 22:17:15 -0400

    Make exception importable at rastervision.filesystem level

--- rastervision/filesystem/__init__.py
+++ rastervision/filesystem/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 
 from rastervision.filesystem.filesystem import (FileSystem, NotReadableError,
-                                                NotWritableError)
+                                                NotWritableError, ProtobufParseException)
 from rastervision.filesystem.local_filesystem import LocalFileSystem
 from rastervision.filesystem.s3_filesystem import S3FileSystem
 from rastervision.filesystem.http_filesystem import HttpFileSystem
