98cde0349335130102500c16ea81bc3951294b80
Lewis Fishgold <lewfish@gmail.com>
2018-11-06 18:40:50 -0500

    Add VectorSource

---
+++ rastervision/data/vector_source/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+from rastervision.data.vector_source.geojson_vector_source import *
+from rastervision.data.vector_source.geojson_vector_source_config import *
+from rastervision.data.vector_source.mbtiles_vector_source import *
+from rastervision.data.vector_source.mbtiles_vector_source_config import *
+from rastervision.data.vector_source.vector_source import *
+from rastervision.data.vector_source.vector_source_config import *
+from rastervision.data.vector_source.class_inference import *
