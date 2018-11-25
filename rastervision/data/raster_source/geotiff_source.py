9a6326f499470d10ebb32dd6313bd3bc5c84fa50
James McClain <jmcclain@azavea.com>
2018-11-25 15:06:01 -0500

    Perform the Shift Correctly

--- rastervision/data/raster_source/geotiff_source.py
+++ rastervision/data/raster_source/geotiff_source.py
@@ -1,14 +1,19 @@
-import subprocess
-import os
 import logging
+import math
+import os
+import pyproj
+import subprocess
 from decimal import Decimal
 
+from rastervision.core.box import Box
+from rastervision.data.crs_transformer import RasterioCRSTransformer
 from rastervision.data.raster_source.rasterio_source \
     import RasterioRasterSource
-from rastervision.data.crs_transformer import RasterioCRSTransformer
 from rastervision.utils.files import download_if_needed
 
 log = logging.getLogger(__name__)
+wgs84 = pyproj.Proj({'init': 'epsg:4326'})
+wgs84_proj4 = '+init=epsg:4326'
 
 
 def build_vrt(vrt_path, image_paths):
@@ -26,48 +31,6 @@ def download_and_build_vrt(image_uris, temp_dir):
     return image_path
 
 
-def modify_geotransform_for_shift(vrt_path, dx, dy):
-    import xml
-    import pyproj
-    import rasterio as rio
-    import math
-
-    tree = xml.etree.ElementTree.parse(vrt_path)
-    geotransform_elt = tree.getroot().find('GeoTransform')
-    a, b, c, d, e, f = list(map(float, geotransform_elt.text.split(',')))
-
-    # Project the upper-left corner coordinates in EPSG 4326
-    with rio.open(vrt_path, 'r') as ds:
-        crs = ds.crs
-    if str(crs) != '+init=epsg:4326':
-        vrtproj = pyproj.Proj(crs)
-        wgs84proj = pyproj.Proj({'init': 'epsg:4326'})
-        lon, lat = pyproj.transform(vrtproj, wgs84proj, a, d)
-    else:
-        lon, lat = a, d
-
-    # Calculate the offsets
-    latr = math.pi * lat / 180.0
-    dx = Decimal(dx) / Decimal(111319.5 * math.cos(latr))
-    dy = Decimal(dy) / Decimal(111319.5)
-
-    # Compute the shifted coordinates
-    lon = float(Decimal(lon) + dx)
-    lat = float(Decimal(lat) + dy)
-
-    # Put the upper-left corner coordinates back into the original CRStransformer
-    if str(crs) != '+init=epsg:4326':
-        new_a, new_d = pyproj.transform(wgs84proj, vrtproj, lon, lat)
-    else:
-        new_a, new_d = lon, lat
-
-    # Edit the VRT
-    geotransform_text = ' {:.16e},  {:.16e},  {:.16e},  {:.16e},  {:.16e},  {:.16e}'.format(  # noqa
-        new_a, b, c, new_d, e, f)
-    geotransform_elt.text = geotransform_text
-    tree.write(vrt_path)
-
-
 class GeoTiffSource(RasterioRasterSource):
     def __init__(self,
                  uris,
@@ -76,24 +39,66 @@ class GeoTiffSource(RasterioRasterSource):
                  channel_order=None,
                  x_shift_meters=0.0,
                  y_shift_meters=0.0):
-        self.uris = uris
         self.x_shift_meters = x_shift_meters
         self.y_shift_meters = y_shift_meters
+        self.uris = uris
         super().__init__(raster_transformers, temp_dir, channel_order)
 
     def _download_data(self, temp_dir):
-        no_shift = self.x_shift_meters == 0.0 and self.y_shift_meters == 0.0
-
-        if len(self.uris) == 1 and no_shift:
+        if len(self.uris) == 1:
             return download_if_needed(self.uris[0], temp_dir)
         else:
-            vrt_path = download_and_build_vrt(self.uris, temp_dir)
-            if not no_shift:
-                dx = self.x_shift_meters
-                dy = self.y_shift_meters
-                modify_geotransform_for_shift(vrt_path, dx, dy)
-            return vrt_path
+            return download_and_build_vrt(self.uris, temp_dir)
 
     def _set_crs_transformer(self):
         self.crs_transformer = RasterioCRSTransformer.from_dataset(
             self.image_dataset)
+
+    def _get_chip(self, window):
+        no_shift = self.x_shift_meters == 0.0 and self.y_shift_meters == 0.0
+        if not no_shift:
+            ymin, xmin, ymax, xmax = window.tuple_format()
+            width = xmax - xmin
+            height = ymax - ymin
+
+            # Transform image coordinates into world coordinates
+            transform = self.image_dataset.transform
+            xmin2, ymin2 = transform * (xmin, ymin)
+
+            # Transform from world coordinates to WGS84
+            if self.crs != wgs84_proj4 and self.proj:
+                lon, lat = pyproj.transform(self.proj, wgs84, xmin2, ymin2)
+            else:
+                lon, lat = xmin2, ymin2
+
+            # Shift
+            lat_radians = math.pi * lat / 180.0
+            dlon = Decimal(self.x_shift_meters) / Decimal(
+                111319.5 * math.cos(lat_radians))
+            dlat = Decimal(self.y_shift_meters) / Decimal(111319.5)
+            lon = float(Decimal(lon) + dlon)
+            lat = float(Decimal(lat) + dlat)
+
+            # Transform from WGS84 to world coordinates
+            if self.crs != wgs84_proj4 and self.proj:
+                xmin3, ymin3 = pyproj.transform(wgs84, self.proj, lon, lat)
+                xmin3 = int(round(xmin3))
+                ymin3 = int(round(ymin3))
+            else:
+                xmin3, ymin3 = lon, lat
+
+            # Trasnform from world coordinates back into image coordinates
+            xmin4, ymin4 = ~transform * (xmin3, ymin3)
+
+            window = Box(ymin4, xmin4, ymin4 + height, xmin4 + width)
+
+        return super()._get_chip(window)
+
+    def _activate(self):
+        super()._activate()
+        self.crs = self.image_dataset.crs
+        if self.crs:
+            self.proj = pyproj.Proj(self.crs)
+        else:
+            self.proj = None
+        self.crs = str(self.crs)
