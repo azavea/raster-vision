5c25425cd5a97e17d0c68b87c8534b2000e78ca7
Lewis Fishgold <lewfish@gmail.com>
2018-12-04 19:04:33 -0500

    Update unit tests

--- tests/data/vector_source/test_vector_tile_vector_source.py
+++ tests/data/vector_source/test_vector_tile_vector_source.py
@@ -3,8 +3,8 @@ import json
 
 from shapely.geometry import shape
 
-from rastervision.data.vector_source import (MBTilesVectorSourceConfigBuilder,
-                                             MBTilesVectorSourceConfig)
+from rastervision.data.vector_source import (
+    VectorTileVectorSourceConfigBuilder, VectorTileVectorSourceConfig)
 from rastervision.core.box import Box
 from rastervision.core.class_map import ClassMap
 from rastervision.data.crs_transformer import IdentityCRSTransformer
@@ -12,32 +12,33 @@ from rastervision.utils.files import file_to_str
 from tests import data_file_path
 
 
-class TestMBTilesVectorSource(unittest.TestCase):
+class TestVectorTileVectorSource(unittest.TestCase):
     def setUp(self):
-        self.uri = data_file_path('vector_tiles/{z}/{x}/{y}.mvt')
         self.class_id_to_filter = {1: ['has', 'building']}
         self.class_map = ClassMap.construct_from(['building'])
         self.crs_transformer = IdentityCRSTransformer()
 
-        b = MBTilesVectorSourceConfigBuilder() \
+    def _get_source(self, uri):
+        b = VectorTileVectorSourceConfigBuilder() \
             .with_class_inference(class_id_to_filter=self.class_id_to_filter,
                                   default_class_id=None) \
-            .with_uri(self.uri) \
+            .with_uri(uri) \
             .with_zoom(14) \
+            .with_id_field('__id') \
             .build()
-
-        self.config = MBTilesVectorSourceConfig.from_proto(b.to_proto())
-
-    def test_get_geojson(self):
+        config = VectorTileVectorSourceConfig.from_proto(b.to_proto())
         aoi_path = data_file_path('vector_tiles/lv-aoi.json')
         extent_geojson = json.loads(file_to_str(aoi_path))
         extent = Box.from_shapely(
             shape(extent_geojson['features'][0]['geometry']))
-        source = self.config.create_source(self.crs_transformer, extent,
-                                           self.class_map)
+        source = config.create_source(self.crs_transformer, extent,
+                                      self.class_map)
+        return source
+
+    def _test_get_geojson(self, vector_tile_uri, json_uri):
+        source = self._get_source(vector_tile_uri)
         geojson = source.get_geojson()
-        expected_geojson = json.loads(
-            file_to_str(data_file_path('vector_tiles/lv.json')))
+        expected_geojson = json.loads(file_to_str(data_file_path(json_uri)))
 
         # Need to convert to JSON and back again because geojson object has tuples
         # instead of lists because of a quirk of shapely.geometry.mapping
@@ -48,6 +49,16 @@ class TestMBTilesVectorSource(unittest.TestCase):
             key=lambda f: f['properties']['__id'])
 
         self.assertDictEqual(geojson, expected_geojson)
+
+    def test_get_geojson_from_zxy(self):
+        vector_tile_uri = data_file_path('vector_tiles/{z}/{x}/{y}.mvt')
+        json_uri = 'vector_tiles/lv-zxy.json'
+        self._test_get_geojson(vector_tile_uri, json_uri)
+
+    def test_get_geojson_from_mbtiles(self):
+        vector_tile_uri = data_file_path('vector_tiles/lv.mbtiles')
+        json_uri = 'vector_tiles/lv-mbtiles.json'
+        self._test_get_geojson(vector_tile_uri, json_uri)
 
 
 if __name__ == '__main__':
