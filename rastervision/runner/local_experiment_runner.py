6ec89c8fd329f962732eead27f5776730f345c09
James McClain <jmcclain@azavea.com>
2018-12-12 13:34:24 -0500

    Use Popen to Run Shell Commands

--- rastervision/runner/local_experiment_runner.py
+++ rastervision/runner/local_experiment_runner.py
@@ -1,8 +1,9 @@
-import subprocess
+from subprocess import Popen
 import sys
 import logging
 
 from rastervision.runner import OutOfProcessExperimentRunner
+from rastervision.utils.misc import (terminate_at_exit)
 
 log = logging.getLogger(__name__)
 
@@ -21,7 +22,10 @@ class LocalExperimentRunner(OutOfProcessExperimentRunner):
                  command,
                  parent_job_ids=None,
                  array_size=None):
-        exitcode = subprocess.call(command, shell=True)
+        command = list(filter(lambda s: len(s) > 0, command.split(' ')))
+        process = Popen(command)
+        terminate_at_exit(process)
+        exitcode = process.wait()
         if exitcode != 0:
             sys.exit(exitcode)
         else:
