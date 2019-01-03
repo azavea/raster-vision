0cc28172e3574f1fec6ce70485f78fcb9e4a6409
James McClain <jmcclain@azavea.com>
2019-01-03 06:15:53 -0500

    Review Comment

--- rastervision/evaluation/classification_evaluator.py
+++ rastervision/evaluation/classification_evaluator.py
@@ -3,8 +3,6 @@ import logging
 
 from rastervision.evaluation import Evaluator
 from rastervision.data import ActivateMixin
-from rastervision.rv_config import RVConfig
-from rastervision.utils.files import (download_if_needed)
 
 log = logging.getLogger(__name__)
 
@@ -39,22 +37,5 @@ class ClassificationEvaluator(Evaluator):
                 scene_evaluation = self.create_evaluation()
                 scene_evaluation.compute(ground_truth, predictions)
                 evaluation.merge(scene_evaluation)
-
-            if hasattr(label_source, 'source') and hasattr(
-                    label_source.source, 'vector_source') and hasattr(
-                        label_store, 'vector_output'):
-                tmp_dir = RVConfig.get_tmp_dir().name
-                gt_geojson = label_source.source.vector_source.uri
-                gt_geojson_local = download_if_needed(gt_geojson, tmp_dir)
-                for vo in label_store.vector_output:
-                    pred_geojson = vo['uri']
-                    mode = vo['mode']
-                    class_id = vo['class_id']
-                    pred_geojson_local = download_if_needed(
-                        pred_geojson, tmp_dir)
-                    scene_evaluation = self.create_evaluation()
-                    scene_evaluation.compute_vector(
-                        gt_geojson_local, pred_geojson_local, mode, class_id)
-                    evaluation.merge(scene_evaluation)
 
         evaluation.save(self.output_uri)
