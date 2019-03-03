fef5b0b35b4e9505ccdfce92fc8f886da50db986
lossyrob <rdemanuele@gmail.com>
2019-03-02 22:02:37 -0500

    Modify chip classification to use pretrained model.

--- integration_tests/chip_classification_tests/experiment.py
+++ integration_tests/chip_classification_tests/experiment.py
@@ -17,9 +17,8 @@ class ChipClassificationIntegrationTest(rv.ExperimentSet):
         backend_conf_path = get_path('configs/backend.config')
 
         pretrained_model = (
-            'https://github.com/fchollet/'
-            'deep-learning-models/releases/download/v0.2/'
-            'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5')
+            'https://github.com/azavea/raster-vision-data/'
+            'releases/download/v0.0.7/chip-classification-test-weights.hdf5')
 
         task = rv.TaskConfig.builder(rv.CHIP_CLASSIFICATION) \
                             .with_chip_size(200) \
