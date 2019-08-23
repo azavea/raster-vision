7efedb81499ca9c1ef16209026414402f0abf33d
lmbak <bakker.laurens@gmail.com>
2019-08-23 17:59:00 +0200

    Added import statements to the augmentor/__init__.py to ensure it imports the mirror augmentor as well.

--- rastervision/augmentor/__init__.py
+++ rastervision/augmentor/__init__.py
@@ -4,3 +4,5 @@ from rastervision.augmentor.augmentor import *
 from rastervision.augmentor.augmentor_config import *
 from rastervision.augmentor.nodata_augmentor import *
 from rastervision.augmentor.nodata_augmentor_config import *
+from rastervision.augmentor.mirror_augmentor import *
+from rastervision.augmentor.mirror_augmentor_config import *
