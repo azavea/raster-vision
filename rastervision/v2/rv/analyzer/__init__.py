cc8906f24f06ba58b36d02b9b952876e6b6263a6
Lewis Fishgold <lewfish@gmail.com>
2020-01-08 16:13:01 -0500

    not used in this pr

--- rastervision/v2/rv/analyzer/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-from rastervision.v2.rv.analyzer.analyzer import *
-from rastervision.v2.rv.analyzer.analyzer_config import *
-from rastervision.v2.rv.analyzer.stats_analyzer import *
-from rastervision.v2.rv.analyzer.stats_analyzer_config import *
--- rastervision/v2/rv/analyzer/api.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# Registry keys
-
-ANALYZER = 'ANALYZER'
-
-STATS_ANALYZER = 'STATS_ANALYZER'
-
-from rastervision.v2.rv.analyzer.analyzer_config import AnalyzerConfig
