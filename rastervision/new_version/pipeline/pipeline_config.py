36123952bb137380dfb53c1e3a79fadd9503d18c
Lewis Fishgold <lewfish@gmail.com>
2020-01-24 14:45:12 -0500

    Delete outdated copy of v2

--- rastervision/new_version/pipeline/pipeline_config.py
+++
@@ -1,9 +0,0 @@
-from rastervision.new_version.pipeline.config import Config
-
-
-class PipelineConfig(Config):
-    root_uri: str
-
-    def get_pipeline(self):
-        from rastervision.new_version.pipeline.pipeline import Pipeline
-        return Pipeline
