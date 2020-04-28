6e8f49968942d662d3c9798593457154e3f39bc3
James McClain <jmcclain@azavea.com>
2020-04-27 21:41:08 -0400

    External Commands in `inprocess` Runner

--- rastervision2/pipeline/runner/inprocess_runner.py
+++ rastervision2/pipeline/runner/inprocess_runner.py
@@ -1,3 +1,6 @@
+import subprocess
+from inspect import signature
+
 from rastervision2.pipeline.cli import _run_command
 from rastervision2.pipeline.runner.runner import Runner
 
@@ -12,8 +15,22 @@ class InProcessRunner(Runner):
 
     def run(self, cfg_json_uri, pipeline, commands, num_splits=1):
         for command in commands:
-            if command in pipeline.split_commands and num_splits > 1:
-                for split_ind in range(num_splits):
-                    _run_command(cfg_json_uri, command, split_ind, num_splits)
+
+            # detect external command
+            if hasattr(pipeline, command):
+                fn = getattr(pipeline, command)
+                params = signature(fn).parameters
+                external = hasattr(fn, 'external') and len(params) == 0
             else:
-                _run_command(cfg_json_uri, command, 0, 1)
+                external = False
+
+            if not external:
+                if command in pipeline.split_commands and num_splits > 1:
+                    for split_ind in range(num_splits):
+                        _run_command(cfg_json_uri, command, split_ind,
+                                     num_splits)
+                else:
+                    _run_command(cfg_json_uri, command, 0, 1)
+            else:
+                cmd = fn()
+                subprocess.run(cmd, check=True)
