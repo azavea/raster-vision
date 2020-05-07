5c46a01afd34d13c55df46b714d3fb826b727e78
jpolchlo <jpolchlopek@azavea.com>
2020-05-07 15:47:48 -0400

    Make types less restrictive

--- rastervision2/core/data/vector_source/vector_source_config.py
+++ rastervision2/core/data/vector_source/vector_source_config.py
@@ -11,7 +11,7 @@ class VectorSourceConfig(Config):
         ('The default class_id to use if class cannot be inferred using other '
          'mechanisms. If a feature defaults to a class_id of None, then that feature '
          'will be deleted.'))
-    class_id_to_filter: Optional[Dict[int, Optional[Sequence[str]]]] = Field(
+    class_id_to_filter: Optional[Dict[int, Optional[Sequence]]] = Field(
         None,
         description=(
             'Map from class_id to JSON filter used to infer missing class_ids. The '
