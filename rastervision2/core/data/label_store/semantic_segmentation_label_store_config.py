94c3bfd93116dd70197075ad69b69eba8a741935
Lewis Fishgold <lewfish@gmail.com>
2020-06-16 12:03:23 -0400

    Only make RGB predictions if that field is set

--- rastervision2/core/data/label_store/semantic_segmentation_label_store_config.py
+++ rastervision2/core/data/label_store/semantic_segmentation_label_store_config.py
@@ -100,7 +100,7 @@ class SemanticSegmentationLabelStoreConfig(LabelStoreConfig):
             crs_transformer,
             tmp_dir,
             vector_output=self.vector_output,
-            class_config=class_config)
+            class_config=class_config if self.rgb else None)
 
     def update(self, pipeline=None, scene=None):
         if pipeline is not None and scene is not None:
