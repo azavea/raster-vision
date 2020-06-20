82f1996c90a36a91d42e0ec3439cca7498b0cf54
Lewis Fishgold <lewfish@gmail.com>
2020-06-20 16:09:57 -0400

    Add plugin aliases for rastervision2
    So old model bundles will still work

--- rastervision/pytorch_backend/__init__.py
+++ rastervision/pytorch_backend/__init__.py
@@ -11,3 +11,4 @@ from rastervision.pytorch_backend.pytorch_object_detection import *
 
 def register_plugin(registry):
     registry.set_plugin_version('rastervision.pytorch_backend', 0)
+    registry.set_plugin_aliases('rastervision.pytorch_backend', ['rastervision2.pytorch_backend'])
