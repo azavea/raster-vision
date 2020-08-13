e2b8ee4432f508e04a099c70d8cbf59c6276be79
Adeel <adeel.hassan93@gmail.com>
2020-08-13 19:20:52 +0500

    refactor

--- rastervision_pytorch_learner/rastervision/pytorch_learner/semantic_segmentation_learner.py
+++ rastervision_pytorch_learner/rastervision/pytorch_learner/semantic_segmentation_learner.py
@@ -23,10 +23,9 @@ from torchvision import models
 from rastervision.pipeline.config import ConfigError
 from rastervision.pytorch_learner.learner import Learner
 from rastervision.pytorch_learner.utils import (
-    compute_conf_mat_metrics, compute_conf_mat, color_to_triple,
-    SplitTensor, Parallel, AddTensors)
+    compute_conf_mat_metrics, compute_conf_mat, color_to_triple, SplitTensor,
+    Parallel, AddTensors)
 from rastervision.pipeline.file_system import make_dir
-
 
 log = logging.getLogger(__name__)
 
@@ -41,16 +40,21 @@ def load_np_normalized(path: Union[IO, str, Path]) -> np.ndarray:
 
 
 class SemanticSegmentationDataset(Dataset):
-    def __init__(self, data_dir: str, img_fmt: str = 'png', label_fmt: str = 'png', transform: Callable = None):
+    def __init__(self,
+                 data_dir: str,
+                 img_fmt: str = 'png',
+                 label_fmt: str = 'png',
+                 transform: Callable = None):
 
         self.data_dir = Path(data_dir)
-        img_dir   = self.data_dir / 'img'
+        img_dir = self.data_dir / 'img'
         label_dir = self.data_dir / 'labels'
 
         # collect image and label paths
         self.img_paths = list(img_dir.glob(f'*.{img_fmt}'))
         self.label_paths = [
-            label_dir / f'{p.stem}.{label_fmt}' for p in self.img_paths]
+            label_dir / f'{p.stem}.{label_fmt}' for p in self.img_paths
+        ]
 
         # choose image loading method based on format
         if img_fmt.lower() in ('npy', 'npz'):
@@ -66,9 +70,10 @@ class SemanticSegmentationDataset(Dataset):
 
         self.transform = transform
 
-    def __getitem__(self, ind: int) -> Tuple[torch.FloatTensor, torch.LongTensor]:
+    def __getitem__(self,
+                    ind: int) -> Tuple[torch.FloatTensor, torch.LongTensor]:
 
-        img_path   = self.img_paths[ind]
+        img_path = self.img_paths[ind]
         label_path = self.label_paths[ind]
 
         x = self.img_load_fn(img_path)
@@ -105,50 +110,45 @@ class SemanticSegmentationLearner(Learner):
 
         input_channels = self.cfg.data.img_channels
         old_conv = model.backbone.conv1
-        old_conv_channels = old_conv.in_channels
 
-        if input_channels == old_conv_channels:
+        if input_channels == old_conv.in_channels:
             return model
 
+        # these parameters will be the same for the new conv layer
+        old_conv_args = {
+            'out_channels': old_conv.out_channels,
+            'kernel_size': old_conv.kernel_size,
+            'stride': old_conv.stride,
+            'padding': old_conv.padding,
+            'dilation': old_conv.dilation,
+            'groups': old_conv.groups,
+            'bias': old_conv.bias
+        }
+
         if not pretrained:
-            # just replace the first conv layer with one with the correct number
-            # of input channels
-            new_conv = torch.nn.Conv2d(
-                in_channels=input_channels,
-                out_channels=old_conv.out_channels,
-                kernel_size=old_conv.kernel_size,
-                stride=old_conv.stride,
-                padding=old_conv.padding,
-                dilation=old_conv.dilation,
-                groups=old_conv.groups,
-                bias=old_conv.bias
-            )
+            # simply replace the first conv layer with one with the
+            # correct number of input channels
+            new_conv = nn.Conv2d(in_channels=input_channels, **old_conv_args)
             model.backbone.conv1 = new_conv
             return model
 
-        if input_channels > old_conv_channels:
+        if input_channels > old_conv.in_channels:
             # insert a new conv layer parallel to the existing one
             # and sum their outputs
-            new_conv_channels = input_channels - old_conv_channels
-            new_conv = torch.nn.Conv2d(
-                in_channels=new_conv_channels,
-                out_channels=old_conv.out_channels,
-                kernel_size=old_conv.kernel_size,
-                stride=old_conv.stride,
-                padding=old_conv.padding,
-                dilation=old_conv.dilation,
-                groups=old_conv.groups,
-                bias=old_conv.bias
-            )
+            new_conv_channels = input_channels - old_conv.in_channels
+            new_conv = nn.Conv2d(
+                in_channels=new_conv_channels, **old_conv_args)
             model.backbone.conv1 = nn.Sequential(
-                SplitTensor(                                        # split input along channel dim
-                    (old_conv_channels, new_conv_channels), dim=1),
-                Parallel(old_conv, new_conv),                       # each split goes to its conv layer
-                AddTensors()                                        # sum the parallel outputs
-            )
+                # split input along channel dim
+                SplitTensor((old_conv.in_channels, new_conv_channels), dim=1),
+                # each split goes to its respective conv layer
+                Parallel(old_conv, new_conv),
+                # sum the parallel outputs
+                AddTensors())
         else:
             raise ConfigError(
-                f'Fewer input channels ({input_channels}) than what the pretrained model expects ({old_conv_channels})')
+                (f'Fewer input channels ({input_channels}) than what'
+                 f'the pretrained model expects ({old_conv.in_channels})'))
 
         return model
 
@@ -167,18 +167,24 @@ class SemanticSegmentationLearner(Learner):
             if isdir(train_dir):
                 tf = transform if cfg.overfit_mode else aug_transform
                 ds = SemanticSegmentationDataset(
-                    train_dir, img_fmt=img_fmt, label_fmt=label_fmt,
+                    train_dir,
+                    img_fmt=img_fmt,
+                    label_fmt=label_fmt,
                     transform=tf)
                 train_ds.append(ds)
 
             if isdir(valid_dir):
                 valid_ds.append(
                     SemanticSegmentationDataset(
-                        valid_dir, img_fmt=img_fmt, label_fmt=label_fmt,
+                        valid_dir,
+                        img_fmt=img_fmt,
+                        label_fmt=label_fmt,
                         transform=transform))
                 test_ds.append(
                     SemanticSegmentationDataset(
-                        valid_dir, img_fmt=img_fmt, label_fmt=label_fmt,
+                        valid_dir,
+                        img_fmt=img_fmt,
+                        label_fmt=label_fmt,
                         transform=transform))
 
         train_ds, valid_ds, test_ds = \
@@ -221,8 +227,11 @@ class SemanticSegmentationLearner(Learner):
     def prob_to_pred(self, x):
         return x.argmax(1)
 
-    def plot_batch(self, x: Union[torch.Tensor, np.ndarray], y: Union[torch.Tensor, np.ndarray],
-                    output_path: str, z: Union[torch.Tensor, np.ndarray] = None) -> None:
+    def plot_batch(self,
+                   x: torch.Tensor,
+                   y: Union[torch.Tensor, np.ndarray],
+                   output_path: str,
+                   z: Optional[torch.Tensor] = None) -> None:
         """Plot a whole batch in a grid using plot_xyz.
 
         Args:
@@ -241,8 +250,10 @@ class SemanticSegmentationLearner(Learner):
             ncols += 1
 
         fig, axes = plt.subplots(
-            nrows=nrows, ncols=ncols,
-            constrained_layout=True, figsize=(3 * ncols, 3 * nrows))
+            nrows=nrows,
+            ncols=ncols,
+            constrained_layout=True,
+            figsize=(3 * ncols, 3 * nrows))
 
         for i in range(batch_sz):
             ax = (fig, axes[i])
@@ -255,45 +266,50 @@ class SemanticSegmentationLearner(Learner):
         plt.savefig(output_path, bbox_inches='tight')
         plt.close()
 
-    def plot_xyz(self, ax: Iterable, x: Union[torch.Tensor, np.ndarray],
-                 y: Union[torch.Tensor, np.ndarray], z: Optional[Union[torch.Tensor, np.ndarray]] = None) -> None:
+    def plot_xyz(self,
+                 ax: Iterable,
+                 x: torch.Tensor,
+                 y: Union[torch.Tensor, np.ndarray],
+                 z: Optional[torch.Tensor] = None) -> None:
 
         channel_groups = self.cfg.data.channel_display_groups
+
+        # make subplot titles
         if not isinstance(channel_groups, dict):
-            channel_groups = {f'channels: {list(chs)}': chs for chs in channel_groups}
+            channel_groups = {
+                f'Channels: {[*chs]}': chs
+                for chs in channel_groups
+            }
 
         fig, ax = ax
-        img_axes = ax[: len(channel_groups)]
+        img_axes = ax[:len(channel_groups)]
         label_ax = ax[len(channel_groups)]
 
         # (c, h, w) --> (h, w, c)
-        if isinstance(x, torch.Tensor):
-            x = x.permute(1, 2, 0)
-        else:
-            x = x.transpose(1, 2, 0)
+        x = x.permute(1, 2, 0)
 
-        # plot images
-        for (name, chs), ax_ in zip(channel_groups.items(), img_axes):
-            if len(chs) == 3:
-                im = x[..., chs]
-            elif len(chs) == 1:
-                im = x[..., chs].expand(-1, -1, 3)
+        # plot input image(s)
+        for (title, chs), ch_ax in zip(channel_groups.items(), img_axes):
+            im = x[..., chs]
+            if len(chs) == 1:
+                im = im.expand(-1, -1, 3)
             elif len(chs) == 2:
                 h, w, _ = x.shape
-                third_channel = torch.full((h, w, 1), .5)
-                im = torch.cat((x[..., chs], third_channel), dim=-1)
-            ax_.imshow(im)
-            ax_.set_title(f'Channels: {name}')
-            ax_.set_xticks([])
-            ax_.set_yticks([])
+                third_channel = torch.full((h, w, 1), fill_value=.5)
+                im = torch.cat((im, third_channel), dim=-1)
+            ch_ax.imshow(im)
+            ch_ax.set_title(title)
+            ch_ax.set_xticks([])
+            ch_ax.set_yticks([])
 
-        class_names = self.cfg.data.class_names
-        colors = [color_to_triple(c) for c in self.cfg.data.class_colors]
-        colors = [tuple([_c / 255 for _c in c]) for c in colors]
+        class_colors = self.cfg.data.class_colors
+        colors = [color_to_triple(c) for c in class_colors]
+        colors = np.array(colors) / 255.
         cmap = matplotlib.colors.ListedColormap(colors)
 
         # plot labels
-        label_ax.imshow(y, vmin=0, vmax=len(colors), cmap=cmap, interpolation='none')
+        label_ax.imshow(
+            y, vmin=0, vmax=len(colors), cmap=cmap, interpolation='none')
         label_ax.set_title(f'Ground truth labels')
         label_ax.set_xticks([])
         label_ax.set_yticks([])
@@ -301,13 +317,19 @@ class SemanticSegmentationLearner(Learner):
         # plot predictions
         if z is not None:
             pred_ax = ax[-1]
-            pred_ax.imshow(z, vmin=0, vmax=len(colors), cmap=cmap, interpolation='none')
+            pred_ax.imshow(
+                z, vmin=0, vmax=len(colors), cmap=cmap, interpolation='none')
             pred_ax.set_title(f'Predicted labels')
             pred_ax.set_xticks([])
             pred_ax.set_yticks([])
 
         # add a legend to the rightmost subplot
-        legend_items = [mpatches.Patch(facecolor=col, edgecolor=(0, 0, 0), label=name)
-                                        for col, name in zip(colors, class_names)]
-        ax[-1].legend(handles=legend_items, loc='center right',
-                      bbox_to_anchor=(1.75, 0.5))
+        class_names = self.cfg.data.class_names
+        legend_items = [
+            mpatches.Patch(facecolor=col, edgecolor='black', label=name)
+            for col, name in zip(colors, class_names)
+        ]
+        ax[-1].legend(
+            handles=legend_items,
+            loc='center right',
+            bbox_to_anchor=(1.8, 0.5))
