17c26a8b586301307253c005aecf2a4c705a8431
Adeel <adeel.hassan93@gmail.com>
2020-08-13 19:24:16 +0500

    propagate img_format & label_format to SampleWriter

--- rastervision_pytorch_backend/rastervision/pytorch_backend/pytorch_semantic_segmentation.py
+++ rastervision_pytorch_backend/rastervision/pytorch_backend/pytorch_semantic_segmentation.py
@@ -4,6 +4,7 @@ import uuid
 import numpy as np
 
 from rastervision.pipeline.file_system import (make_dir)
+from rastervision.core.data import ClassConfig
 from rastervision.core.data.label import SemanticSegmentationLabels
 from rastervision.core.utils.misc import save_img
 from rastervision.core.data_sample import DataSample
@@ -12,6 +13,27 @@ from rastervision.pytorch_backend.pytorch_learner_backend import (
 
 
 class PyTorchSemanticSegmentationSampleWriter(PyTorchLearnerSampleWriter):
+    def __init__(self,
+                 output_uri: str,
+                 class_config: ClassConfig,
+                 tmp_dir: str,
+                 img_format: str = 'png',
+                 label_format: str = 'png'):
+        """Constructor.
+
+        Args:
+            output_uri: URI of directory where zip file of chips should be placed
+            class_config: used to convert class ids to names which may be needed for some
+                training data formats
+            tmp_dir: local directory which is root of any temporary directories that
+                are created
+            img_format: file format to store the image in
+            label_format: file format to store the labels in
+        """
+        super().__init__(output_uri, class_config, tmp_dir)
+        self.img_format = img_format
+        self.label_format = label_format
+
     def write_sample(self, sample: DataSample):
         """
         This writes a training or validation sample to
@@ -22,33 +44,37 @@ class PyTorchSemanticSegmentationSampleWriter(PyTorchLearnerSampleWriter):
         label_arr = sample.labels.get_label_arr(sample.window).astype(np.uint8)
 
         img_dir = join(self.sample_dir, split_name, 'img')
-        labels_dir = join(self.sample_dir, split_name, 'labels')
+        label_dir = join(self.sample_dir, split_name, 'labels')
         make_dir(img_dir)
-        make_dir(labels_dir)
+        make_dir(label_dir)
 
+        img_fmt, label_fmt = self.img_format, self.label_format
         num_channels = sample.chip.shape[-1]
-        if num_channels > 3:
-            img_name = '{}-{}'.format(sample.scene_id, self.sample_ind)
+        if img_fmt == 'npy':
+            img_name = f'{sample.scene_id}-{self.sample_ind}.npy'
             img_path = join(img_dir, img_name)
             np.save(img_path, sample.chip)
         else:
-            img_name = '{}-{}.png'.format(sample.scene_id, self.sample_ind)
+            img_name = f'{sample.scene_id}-{self.sample_ind}.{img_fmt}'
             img_path = join(img_dir, img_name)
             save_img(sample.chip, img_path)
 
-        labels_path = join(
-            labels_dir, '{}-{}.png'.format(sample.scene_id, self.sample_ind))
-        save_img(label_arr, labels_path)
+        label_name = f'{sample.scene_id}-{self.sample_ind}.{label_fmt}'
+        label_path = join(label_dir, label_name)
+        save_img(label_arr, label_path)
 
         self.sample_ind += 1
 
 
 class PyTorchSemanticSegmentation(PyTorchLearnerBackend):
     def get_sample_writer(self):
-        output_uri = join(self.pipeline_cfg.chip_uri, '{}.zip'.format(
-            str(uuid.uuid4())))
+        output_uri = join(self.pipeline_cfg.chip_uri, f'{uuid.uuid4()}.zip')
         return PyTorchSemanticSegmentationSampleWriter(
-            output_uri, self.pipeline_cfg.dataset.class_config, self.tmp_dir)
+            output_uri,
+            self.pipeline_cfg.dataset.class_config,
+            self.tmp_dir,
+            img_format=self.pipeline_cfg.img_format,
+            label_format=self.pipeline_cfg.label_format)
 
     def predict(self, chips, windows):
         if self.learner is None:
