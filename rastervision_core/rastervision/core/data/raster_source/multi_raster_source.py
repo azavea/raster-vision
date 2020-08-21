4e41438e1bf8bc52f286e0f62125c66e3b20f8ff
ahassan@azavea.com <adeel.hassan93@gmail.com>
2020-08-21 20:28:37 +0500

    more implementation

--- rastervision_core/rastervision/core/data/raster_source/multi_raster_source.py
+++ rastervision_core/rastervision/core/data/raster_source/multi_raster_source.py
@@ -14,30 +14,58 @@ from rastervision.pipeline.file_system import download_if_needed
 from rastervision.core.box import Box
 from rastervision.core.data.raster_source import RasterSource
 from rastervision.core.data import (ActivateMixin, ActivationError)
+from rastervision.core.data.crs_transformer import CRSTransformer
+from rastervision.core.data.utils import all_equal
+
+
+class MultiRasterSourceError(Exception):
+    pass
 
 
 class MultiRasterSource(ActivateMixin, RasterSource):
+    def __init__(self,
+                 raster_sources,
+                 raw_channel_order,
+                 raster_transformers=[]):
+        super().__init__([], len(raw_channel_order), raster_transformers)
+        self.raster_sources = raster_sources
+        self.raw_channel_order = raw_channel_order
+        self.num_channels = len(self.raw_channel_order)
+        self.crs_transformer = CRSTransformer()
+        self.validate_raster_sources()
 
-	def __init__(self, raster_sources, raster_transformers=[]):
-		self.raster_sources = raster_sources
-        self.raster_transformers = raster_transformers
+    def validate_raster_sources(self):
+        dtypes = [rs.get_dtype() for rs in self.raster_sources]
+        if not all_equal(dtypes):
+            raise MultiRasterSourceError(
+                'dtypes of all sub raster sources must be same')
+
+        num_channels = sum(rs.num_channels for rs in self.raster_sources)
+        if num_channels != self.num_channels:
+            raise MultiRasterSourceError(
+                'num_channels and channel mappings for sub raster sources do '
+                'not match')
 
     def _activate(self):
         for rs in self.raster_sources:
-			rs._activate()
+            rs._activate()
 
     def _deactivate(self):
         for rs in self.raster_sources:
-			rs._deactivate()
+            rs._deactivate()
 
     def get_extent(self):
-        raise NotImplementedError()
+        rs = self.raster_sources[0]
+        extent = rs.get_extent()
+        return extent
 
     def get_dtype(self):
-        raise NotImplementedError()
+        rs = self.raster_sources[0]
+        dtype = rs.get_dtype()
+        return dtype
 
     def get_crs_transformer(self):
-        raise NotImplementedError()
+        return self.crs_transformer
 
     def _get_chip(self, window):
         """Return the raw chip located in the window.
@@ -49,8 +77,9 @@ class MultiRasterSource(ActivateMixin, RasterSource):
             [height, width, channels] numpy array
         """
         chip_slices = [rs._get_chip(window) for rs in self.raster_sources]
-		chip = np.concatenate(chip_slices, axis=-1)
-		return chip
+        chip = np.concatenate(chip_slices, axis=-1)
+        chip = chip[..., self.raw_channel_order]
+        return chip
 
     def get_raw_chip(self, window):
         """Return raw chip without using channel_order or applying transforms.
@@ -63,16 +92,18 @@ class MultiRasterSource(ActivateMixin, RasterSource):
         """
         return self._get_chip(window)
 
-    def get_image_array(self):
-        """Return entire transformed image array.
-
-        Not safe to call on very large RasterSources.
-        """
-        raise NotImplementedError()
-
     def get_raw_image_array(self):
         """Return entire raw image without using channel_order or applying transforms.
 
         Not safe to call on very large RasterSources.
         """
-        raise NotImplementedError()
+        window = self.get_extent()
+        return self.get_raw_chip(window)
+
+    def get_image_array(self):
+        """Return entire transformed image array.
+
+        Not safe to call on very large RasterSources.
+        """
+        window = self.get_extent()
+        return self.get_chip(window)
--- rastervision_core/rastervision/core/data/raster_source/multi_raster_source_config.py
+++ rastervision_core/rastervision/core/data/raster_source/multi_raster_source_config.py
@@ -1,26 +1,63 @@
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Union
 
-from rastervision.pipeline.config import Config, register_config, Field
+import numpy as np
+
+from rastervision.pipeline.config import (Config, register_config, Field,
+                                          ConfigError)
 from rastervision.core.data.raster_transformer import RasterTransformerConfig
-from rastervision.core.data.raster_source import RasterSourceConfig, MultiRasterSource
+from rastervision.core.data.raster_source import RasterSourceConfig, RasterioSourceConfig, MultiRasterSource
 
 
-@register_config('raster_source')
+@register_config('multi_raster_source')
 class MultiRasterSourceConfig(RasterSourceConfig):
-	# TODO add descriptions
-	raster_source_configs: List[Tuple[RasterSourceConfig, tuple]] = Field([], description='')
-	raw_channel_order: Optional[List[int]] = Field(None, description='')
+    raster_sources: List[Tuple[RasterSourceConfig, Union[tuple, List[
+        int]]]] = Field(
+            ...,
+            description='List of (RasterSourceConfig, indices) pair, where '
+            'are the positions to which each channel of the raster source will '
+            'be sent.')
 
-	def update(self):
-		self.raw_channel_order = sum(list(cfg[1]) for cfg in self.raster_source_configs], [])
+    def get_raw_channel_order(self):
+        channel_mappings = sum((list(inds) for _, inds in self.raster_sources),
+                               [])
+        self.validate_channel_mappings(channel_mappings)
+        raw_channel_order = np.argsort(channel_mappings)
+
+        return raw_channel_order
+
+    def validate_channel_mappings(self, channel_mappings):
+        # ensure we have a source channel for each channel idx
+        if set(channel_mappings) != set(range(len(channel_mappings))):
+            raise ConfigError(f'Missing mappings for some channels.')
+
+    def validate_config(self):
+        super().validate_config()
+
+        if len(self.raster_sources) == 0:
+            raise ConfigError(f'No raster sources provided.')
+
+        for i, (_, inds) in enumerate(self.raster_sources):
+            if len(inds) == 0:
+                raise ConfigError(
+                    f'Got no indices for raster source at index {i}.')
+            if isinstance(inds, tuple):
+                if any(not isinstance(ind, int) for ind in inds):
+                    raise ConfigError(f'Indices must be ints. Got: {ind}.')
+            if any(ind < 0 for ind in inds):
+                raise ConfigError(f'Indices must be >= 0. Got: {ind}.')
 
     def build(self, tmp_dir, use_transformers=True):
         raster_transformers = ([rt.build() for rt in self.transformers]
                                if use_transformers else [])
 
-		raster_sources = [cfg.build(tmp_dir, use_transformers) for cfg in self.raster_source_configs]
-		multi_raster_source = MultiRasterSource(raster_sources, raster_transformers)
-		return multi_raster_source
+        raster_sources = [
+            cfg.build(tmp_dir, use_transformers) for cfg, _ in self.raster_sources
+        ]
+        multi_raster_source = MultiRasterSource(
+            raster_sources=raster_sources,
+            raw_channel_order=self.get_raw_channel_order(),
+            raster_transformers=raster_transformers)
+        return multi_raster_source
 
     def update(self, pipeline=None, scene=None):
         for t in self.transformers:
