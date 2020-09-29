2e520af38ba3909aa24ae0edeb4b4a4f322044b3
James McClain <jmcclain@azavea.com>
2020-09-29 13:37:29 -0400

    Add NaN Transformer

--- rastervision_core/rastervision/core/data/raster_transformer/__init__.py
+++ rastervision_core/rastervision/core/data/raster_transformer/__init__.py
@@ -4,3 +4,5 @@ from rastervision.core.data.raster_transformer.raster_transformer import *
 from rastervision.core.data.raster_transformer.raster_transformer_config import *
 from rastervision.core.data.raster_transformer.stats_transformer import *
 from rastervision.core.data.raster_transformer.stats_transformer_config import *
+from rastervision.core.data.raster_transformer.nan_transformer import *
+from rastervision.core.data.raster_transformer.nan_transformer_config import *
