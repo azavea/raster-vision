00eefc2da3b2ba3a6f1e90d47dc771b01e40932c
James McClain <jmcclain@azavea.com>
2020-11-18 12:35:44 -0500

    Remove External Commands from Local Runner

--- rastervision_pipeline/rastervision/pipeline/runner/local_runner.py
+++ rastervision_pipeline/rastervision/pipeline/runner/local_runner.py
@@ -1,5 +1,4 @@
 import sys
-from inspect import signature
 from os.path import dirname, join
 from subprocess import Popen
 
@@ -41,61 +40,29 @@ class LocalRunner(Runner):
         curr_command_ind = 0
         for command in commands:
 
-            # detect external command
-            if hasattr(pipeline, command):
-                fn = getattr(pipeline, command)
-                params = signature(fn).parameters
-                external = hasattr(fn, 'external') and len(params) in {0, 1}
-            else:
-                external = False
-
             curr_command_inds = []
-            if not external:
-                if command in pipeline.split_commands and num_splits > 1:
-                    for split_ind in range(num_splits):
-                        makefile += '{}: '.format(curr_command_ind)
-                        makefile += ' '.join(
-                            [str(ci) for ci in prev_command_inds])
-                        makefile += '\n'
-                        invocation = (
-                            'python -m rastervision.pipeline.cli run_command '
-                            '{} {} --split-ind {} --num-splits {}').format(
-                                cfg_json_uri, command, split_ind, num_splits)
-                        makefile += '\t{}\n\n'.format(invocation)
-                        curr_command_inds.append(curr_command_ind)
-                        curr_command_ind += 1
-                else:
+            if command in pipeline.split_commands and num_splits > 1:
+                for split_ind in range(num_splits):
                     makefile += '{}: '.format(curr_command_ind)
                     makefile += ' '.join([str(ci) for ci in prev_command_inds])
                     makefile += '\n'
                     invocation = (
                         'python -m rastervision.pipeline.cli run_command '
-                        '{} {}'.format(cfg_json_uri, command))
+                        '{} {} --split-ind {} --num-splits {}').format(
+                            cfg_json_uri, command, split_ind, num_splits)
                     makefile += '\t{}\n\n'.format(invocation)
                     curr_command_inds.append(curr_command_ind)
                     curr_command_ind += 1
             else:
-                if len(params) == 0:
-                    # No-parameter external command
-                    cmds = [fn()]
-                elif len(params) == 1 and command in pipeline.split_commands:
-                    # One-paramater split external command
-                    cmds = fn(num_splits)
-                elif len(params
-                         ) == 1 and command not in pipeline.split_commands:
-                    # One-paramater unsplit external command
-                    cmds = fn(1)
-                else:
-                    # No command
-                    cmds = []
-                for cmd in cmds:
-                    makefile += '{}: '.format(curr_command_ind)
-                    makefile += ' '.join([str(ci) for ci in prev_command_inds])
-                    makefile += '\n'
-                    invocation = (' '.join(cmd))
-                    makefile += '\t{}\n\n'.format(invocation)
-                    curr_command_inds.append(curr_command_ind)
-                    curr_command_ind += 1
+                makefile += '{}: '.format(curr_command_ind)
+                makefile += ' '.join([str(ci) for ci in prev_command_inds])
+                makefile += '\n'
+                invocation = (
+                    'python -m rastervision.pipeline.cli run_command '
+                    '{} {}'.format(cfg_json_uri, command))
+                makefile += '\t{}\n\n'.format(invocation)
+                curr_command_inds.append(curr_command_ind)
+                curr_command_ind += 1
 
             prev_command_inds = curr_command_inds
 
