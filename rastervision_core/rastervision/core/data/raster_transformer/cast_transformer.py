da9b3c296b57940b6ddb4d299e21ae9735e35dd2
ahassan@azavea.com <adeel.hassan93@gmail.com>
2020-12-24 18:05:46 +0500

    docs and type fixes

--- rastervision_core/rastervision/core/data/raster_transformer/cast_transformer.py
+++ rastervision_core/rastervision/core/data/raster_transformer/cast_transformer.py
@@ -1,3 +1,4 @@
+from typing import Optional
 
 from rastervision.core.data.raster_transformer.raster_transformer \
     import RasterTransformer
@@ -6,29 +7,24 @@ import numpy as np
 
 
 class CastTransformer(RasterTransformer):
-    """Removes Cast values from float raster
-    """
+    """ Casts chips to the specified dtype. """
 
     def __init__(self, to_dtype: str):
-        """Construct a new CastTransformer.
+        """Constructor.
 
         Args:
-            to_dtype: (str) Chips are casted to this dtype
+            to_dtype: (str) dtype to cast the chips to.
         """
         self.to_dtype = np.dtype(to_dtype)
 
-    def transform(self, chip, channel_order=None):
-        """Transform a chip.
-
-        Cast chip to the specified dtype.
+    def transform(self, chip: np.ndarray,
+                  channel_order: Optional[list] = None) -> np.ndarray:
+        """Cast chip to self.to_dtype.
 
         Args:
-            chip: ndarray of shape [height, width, channels] This is assumed to already
-                have the channel_order applied to it if channel_order is set. In other
-                words, channels should be equal to len(channel_order).
+            chip: ndarray of shape [height, width, channels]
 
         Returns:
             [height, width, channels] numpy array
-
         """
         return chip.astype(self.to_dtype)
