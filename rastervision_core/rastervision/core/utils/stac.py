f21c26e9334cbb1cc1295ecb21cbf77173e17b92
ahassan@azavea.com <adeel.hassan93@gmail.com>
2021-09-08 13:22:56 +0500

    make compatible with stac v1.0.*

--- rastervision_core/rastervision/core/utils/stac.py
+++ rastervision_core/rastervision/core/utils/stac.py
@@ -1,13 +1,19 @@
+from typing import List, Optional
 from urllib.parse import urlparse
+import logging
 
 import boto3
-from pystac import STAC_IO
-from pystac import Catalog, LabelItem
+from pystac import StacIO, Catalog, Item
+from pystac.stac_io import DefaultStacIO
 from shapely.geometry import box
 
+from rastervision.pipeline.file_system import file_to_json
 
-def setup_stac_s3():
-    def my_read_method(uri):
+log = logging.getLogger(__name__)
+
+
+def setup_stac_io() -> None:
+    def read_method(uri: str):
         parsed = urlparse(uri)
         if parsed.scheme == 's3':
             bucket = parsed.netloc
@@ -16,9 +22,9 @@ def setup_stac_s3():
             obj = s3.Object(bucket, key)
             return obj.get()['Body'].read().decode('utf-8')
         else:
-            return STAC_IO.default_read_text_method(uri)
+            return DefaultStacIO.read_text_method(uri)
 
-    def my_write_method(uri, txt):
+    def write_method(uri: str, txt: str):
         parsed = urlparse(uri)
         if parsed.scheme == 's3':
             bucket = parsed.netloc
@@ -26,34 +32,91 @@ def setup_stac_s3():
             s3 = boto3.resource('s3')
             s3.Object(bucket, key).put(Body=txt)
         else:
-            STAC_IO.default_write_text_method(uri, txt)
+            DefaultStacIO.write_text_method(uri, txt)
 
-    STAC_IO.read_text_method = my_read_method
-    STAC_IO.write_text_method = my_write_method
+    StacIO.read_text_method = read_method
+    StacIO.write_text_method = write_method
 
 
-def parse_stac(stac_uri):
-    setup_stac_s3()
+def is_label_item(item: Item) -> bool:
+    """Resolve each extension schema into a dict, then check if has the title
+    of "Label Extension".
+    """
+    for ext_schema_uri in item.stac_extensions:
+        schema = file_to_json(ext_schema_uri)
+        if schema['title'].lower() == 'label extension':
+            return True
+    return False
+
+
+def get_linked_image_item(label_item: Item) -> Optional[Item]:
+    """Find link in the item that have "rel" == "source" and return its
+    "target" item. If no such link, return None. If multiple such links,
+    raise an exception."""
+    links = [l for l in label_item.links if l.rel.lower() == 'source']
+    if len(links) == 0:
+        return None
+    elif len(links) > 1:
+        raise NotImplementedError()
+    image_item = links[0].resolve_stac_object().target
+    return image_item
+
+
+def parse_stac(stac_uri: str) -> List[dict]:
+    """Parse a STAC catalog JSON file to extract label URIs, images URIs,
+    and AOIs.
+
+    Note: This is has been tested to be compatible with STAC version 1.0.0 but
+    not any other versions.
+
+    Args:
+        stac_uri (str): Path to the STAC catalog JSON file.
+
+    Returns:
+        List[dict]: A lsit of dicts with keys: "label_uri", "image_uris",
+            "label_bbox", "image_bbox", and "aoi_geometry".
+    """
+    setup_stac_io()
     cat = Catalog.from_file(stac_uri)
+    version: str = cat.to_dict()['stac_version']
+
+    if not version.startswith('1.0'):
+        log.warning(f'Parsing is not guaranteed to work correctly for '
+                    f'STAC version != 1.0.*. Found version: {version}.')
+
     cat.make_all_asset_hrefs_absolute()
-    labels_uri = None
-    geotiff_uris = []
-    for item in cat.get_all_items():
-        if isinstance(item, LabelItem):
-            labels_uri = list(item.assets.values())[0].href
-            labels_box = box(*item.bbox)
 
-    # only use geotiffs that intersect with bbox of labels
-    for item in cat.get_all_items():
-        if not isinstance(item, LabelItem):
-            geotiff_uri = list(item.assets.values())[0].href
-            geotiff_box = box(*item.bbox)
-            if labels_box.intersects(geotiff_box):
-                geotiff_uri = geotiff_uri.replace('%7C', '|')
-                geotiff_uris.append(geotiff_uri)
+    label_items = [item for item in cat.get_all_items() if is_label_item(item)]
+    image_items = [get_linked_image_item(item) for item in label_items]
 
-    if not labels_uri:
-        raise ValueError('Unable to read labels URI from STAC.')
-    if not geotiff_uris:
-        raise ValueError('Unable to read GeoTIFF URIs from STAC.')
-    return labels_uri, labels_box, geotiff_uris
+    if len(label_items) == 0:
+        raise ValueError('Unable to find any label items in STAC catalog.')
+
+    out = []
+    for label_item, image_item in zip(label_items, image_items):
+        label_uri: str = list(label_item.assets.values())[0].href
+        label_bbox = box(*label_item.bbox)
+        aoi_geometry = label_item.geometry
+
+        if image_item is not None:
+            image_assets = [
+                asset for asset in image_item.get_assets().values()
+                if 'image' in asset.media_type
+            ]
+            image_uris = [asset.href for asset in image_assets]
+            image_bbox = box(*image_item.bbox)
+            bboxes_intersect = label_bbox.intersects(image_bbox)
+        else:
+            image_uris = []
+            image_bbox = None
+            bboxes_intersect = False
+
+        out.append({
+            'label_uri': label_uri,
+            'image_uris': image_uris,
+            'label_bbox': label_bbox,
+            'image_bbox': image_bbox,
+            'bboxes_intersect': bboxes_intersect,
+            'aoi_geometry': aoi_geometry
+        })
+    return out
