69d46664b280a7df95a16f3c6422025d21f3078a
ahassan@azavea.com <adeel.hassan93@gmail.com>
2022-02-03 15:27:08 +0500

    add new unit test for multiRS with non-identical extents and resolutions
    also adds 3 small tiff files for use in the test

--- tests/core/data/raster_source/test_multi_raster_source.py
+++ tests/core/data/raster_source/test_multi_raster_source.py
@@ -1,6 +1,9 @@
 import unittest
 from pydantic import ValidationError
 
+import numpy as np
+
+from rastervision.core.box import Box
 from rastervision.core.data import (RasterioSourceConfig,
                                     MultiRasterSourceConfig,
                                     SubRasterSourceConfig, CropOffsets)
@@ -93,6 +96,36 @@ class TestRasterioSource(unittest.TestCase):
             _ = make_cfg('small-rgb-tile.tif', extent_crop=None)  # noqa
         except Exception:
             self.fail('extent_crop=None caused an error.')
+
+    def test_nonidentical_extents_and_resolutions(self):
+        img_path_1 = data_file_path(
+            'multi_raster_source/const_100_600x600.tiff')
+        img_path_2 = data_file_path('multi_raster_source/const_175_60x60.tiff')
+        img_path_3 = data_file_path('multi_raster_source/const_250_6x6.tiff')
+        source_1 = RasterioSourceConfig(uris=[img_path_1], channel_order=[0])
+        source_2 = RasterioSourceConfig(uris=[img_path_2], channel_order=[0])
+        source_3 = RasterioSourceConfig(uris=[img_path_3], channel_order=[0])
+
+        cfg = MultiRasterSourceConfig(raster_sources=[
+            SubRasterSourceConfig(raster_source=source_1, target_channels=[0]),
+            SubRasterSourceConfig(raster_source=source_2, target_channels=[1]),
+            SubRasterSourceConfig(raster_source=source_3, target_channels=[2])
+        ])
+        rs = cfg.build(tmp_dir=self.tmp_dir)
+        with rs.activate():
+            ch_1_only = rs.get_chip(Box(0, 0, 10, 10))
+            self.assertEqual(
+                tuple(ch_1_only.reshape(-1, 3).mean(axis=0)), (100, 0, 0))
+            ch_2_only = rs.get_chip(Box(0, 600, 10, 600 + 10))
+            self.assertEqual(
+                tuple(ch_2_only.reshape(-1, 3).mean(axis=0)), (0, 175, 0))
+            ch_3_only = rs.get_chip(Box(600, 0, 600 + 10, 10))
+            self.assertEqual(
+                tuple(ch_3_only.reshape(-1, 3).mean(axis=0)), (0, 0, 250))
+            full_img = rs.get_chip(Box(0, 0, 600, 600))
+            self.assertEqual(set(np.unique(full_img[..., 0])), {100})
+            self.assertEqual(set(np.unique(full_img[..., 1])), {0, 175})
+            self.assertEqual(set(np.unique(full_img[..., 2])), {0, 250})
 
 
 if __name__ == '__main__':
