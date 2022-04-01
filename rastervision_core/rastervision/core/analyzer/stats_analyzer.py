f08396da0ad5b613be01e5e8057611064fa92318
ahassan@azavea.com <adeel.hassan93@gmail.com>
2022-04-01 15:08:09 +0500

    refactor StatsAnalyzer
    make it usable without having to save stats to file

--- rastervision_core/rastervision/core/analyzer/stats_analyzer.py
+++ rastervision_core/rastervision/core/analyzer/stats_analyzer.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Iterable, Optional
 
 from rastervision.core.analyzer import Analyzer
 from rastervision.core.raster_stats import RasterStats
@@ -8,12 +8,21 @@ from rastervision.core.data import Scene
 class StatsAnalyzer(Analyzer):
     """Computes RasterStats against the entire scene set."""
 
-    def __init__(self, stats_uri: str, sample_prob: float = 0.1):
+    def __init__(self,
+                 stats_uri: Optional[str] = None,
+                 sample_prob: float = 0.1):
         self.stats_uri = stats_uri
         self.sample_prob = sample_prob
 
-    def process(self, scenes: List[Scene], tmp_dir: str):
+    def compute_stats(self, scenes: Iterable[Scene]) -> RasterStats:
         stats = RasterStats()
         stats.compute(
             [s.raster_source for s in scenes], sample_prob=self.sample_prob)
-        stats.save(self.stats_uri)
+        return stats
+
+    def process(self, scenes: Iterable[Scene], tmp_dir: str) -> None:
+        stats = self.compute_stats(scenes)
+        if self.stats_uri is not None:
+            stats.save(self.stats_uri)
+        else:
+            raise ValueError('Cannot save stats because stats_uri is not set.')
