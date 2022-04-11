d5ff0b7e214fa036b703c199d8dc0ece394b126f
ahassan@azavea.com <adeel.hassan93@gmail.com>
2022-04-11 19:17:49 +0500

    update unit tests

--- tests/core/evaluation/test_class_evaluation_item.py
+++ tests/core/evaluation/test_class_evaluation_item.py
@@ -1,5 +1,7 @@
 import unittest
 
+import numpy as np
+
 from rastervision.core.evaluation import ClassEvaluationItem
 
 
@@ -7,49 +9,113 @@ class TestClassEvaluationItem(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_merge_both_empty(self):
-        a = ClassEvaluationItem()
-        b = ClassEvaluationItem()
-        a.merge(b)
-        self.assertEqual(a.precision, None)
-        self.assertEqual(a.recall, None)
-        self.assertEqual(a.f1, None)
-        self.assertEqual(a.count_error, None)
-        self.assertEqual(a.gt_count, 0)
+    def test_with_tn(self):
+        conf_mat = np.random.randint(100, size=(2, 2))
+        [[tn, fp], [fn, tp]] = conf_mat
+        item = ClassEvaluationItem(
+            class_id=0, class_name='abc', tp=tp, fp=fp, fn=fn, tn=tn)
+        self.assertEqual(item.true_pos, tp)
+        self.assertEqual(item.true_neg, tn)
+        self.assertEqual(item.false_pos, fp)
+        self.assertEqual(item.false_neg, fn)
 
-    def test_merge_first_empty(self):
-        a = ClassEvaluationItem()
-        b = ClassEvaluationItem(
-            precision=1, recall=1, f1=1, count_error=0, gt_count=1)
-        a.merge(b)
-        self.assertEqual(a.precision, 1)
-        self.assertEqual(a.recall, 1)
-        self.assertEqual(a.f1, 1)
-        self.assertEqual(a.count_error, 0)
-        self.assertEqual(a.gt_count, 1)
+        recall = tp / (tp + fn)
+        precision = tp / (tp + fp)
+        self.assertEqual(item.gt_count, fn + tp)
+        self.assertEqual(item.pred_count, fp + tp)
+        self.assertEqual(item.recall, recall)
+        self.assertEqual(item.sensitivity, recall)
+        self.assertEqual(item.precision, precision)
+        self.assertEqual(item.specificity, tn / (tn + fp))
+        self.assertEqual(item.f1,
+                         2 * (precision * recall) / (precision + recall))
 
-    def test_merge_second_empty(self):
-        a = ClassEvaluationItem(
-            precision=1, recall=1, f1=1, count_error=0, gt_count=1)
-        b = ClassEvaluationItem()
-        a.merge(b)
-        self.assertEqual(a.precision, 1)
-        self.assertEqual(a.recall, 1)
-        self.assertEqual(a.f1, 1)
-        self.assertEqual(a.count_error, 0)
-        self.assertEqual(a.gt_count, 1)
+        json = item.to_json()
+        self.assertEqual(json['relative_frequency'],
+                         (fn + tp) / (fn + tp + fp + tn))
+        self.assertEqual(json['count_error'], abs((fn + tp) - (fp + tp)))
+        np.testing.assert_array_equal(np.array(json['conf_mat']), conf_mat)
+
+    def test_without_tn(self):
+        conf_mat = np.random.randint(100, size=(2, 2))
+        [[_, fp], [fn, tp]] = conf_mat
+        tn = None
+        item = ClassEvaluationItem(
+            class_id=0, class_name='abc', tp=tp, fp=fp, fn=fn, tn=tn)
+        self.assertEqual(item.true_pos, tp)
+        self.assertEqual(item.true_neg, None)
+        self.assertEqual(item.false_pos, fp)
+        self.assertEqual(item.false_neg, fn)
+
+        recall = tp / (tp + fn)
+        precision = tp / (tp + fp)
+        self.assertEqual(item.gt_count, fn + tp)
+        self.assertEqual(item.pred_count, fp + tp)
+        self.assertEqual(item.recall, recall)
+        self.assertEqual(item.sensitivity, recall)
+        self.assertEqual(item.precision, precision)
+        self.assertEqual(item.specificity, None)
+        self.assertEqual(item.f1,
+                         2 * (precision * recall) / (precision + recall))
+
+        json = item.to_json()
+        self.assertNotIn('relative_frequency', json)
+        self.assertIsNone(json['metrics']['specificity'])
+        self.assertEqual(json['true_pos'], tp)
+        self.assertEqual(json['false_pos'], fp)
+        self.assertEqual(json['false_neg'], fn)
 
     def test_merge(self):
-        a = ClassEvaluationItem(
-            precision=1, recall=1, f1=1, count_error=0, gt_count=1)
-        b = ClassEvaluationItem(
-            precision=0, recall=0, f1=0, count_error=1, gt_count=2)
-        a.merge(b)
-        self.assertEqual(a.precision, 1 / 3)
-        self.assertEqual(a.recall, 1 / 3)
-        self.assertEqual(a.f1, 1 / 3)
-        self.assertEqual(a.count_error, 2 / 3)
-        self.assertEqual(a.gt_count, 3)
+        conf_mat1 = np.random.randint(100, size=(2, 2))
+        [[tn, fp], [fn, tp]] = conf_mat1
+        item1 = ClassEvaluationItem(
+            class_id=0, class_name='abc', tp=tp, fp=fp, fn=fn, tn=tn)
+
+        conf_mat2 = np.random.randint(100, size=(2, 2))
+        [[tn, fp], [fn, tp]] = conf_mat2
+        item2 = ClassEvaluationItem(
+            class_id=0, class_name='abc', tp=tp, fp=fp, fn=fn, tn=tn)
+
+        item1.merge(item2)
+        np.testing.assert_array_equal(item1.conf_mat, conf_mat1 + conf_mat2)
+
+    def test_extra_info(self):
+        conf_mat = np.random.randint(100, size=(2, 2))
+        [[tn, fp], [fn, tp]] = conf_mat
+        item = ClassEvaluationItem(
+            class_id=0,
+            class_name='abc',
+            tp=tp,
+            fp=fp,
+            fn=fn,
+            tn=tn,
+            extra1='extra1',
+            extra2='extra2')
+        json = item.to_json()
+        self.assertEqual(json['extra1'], 'extra1')
+        self.assertEqual(json['extra2'], 'extra2')
+
+    def from_multiclass_conf_mat(self):
+        conf_mat = np.random.randint(100, size=(10, 10))
+        item = ClassEvaluationItem.from_multiclass_conf_mat(
+            class_id=3,
+            class_name='abc',
+            conf_mat=conf_mat,
+            extra1='extra1',
+            extra2='extra2')
+
+        tp = conf_mat[3, 3]
+        fp = conf_mat[:, 3].sum() - tp
+        fn = conf_mat[3, :].sum() - tp
+        tn = conf_mat.sum() - tp - fp - fn
+        self.assertEqual(item.true_pos, tp)
+        self.assertEqual(item.false_pos, fp)
+        self.assertEqual(item.false_neg, fn)
+        self.assertEqual(item.true_neg, tn)
+
+        json = item.to_json()
+        self.assertEqual(json['extra1'], 'extra1')
+        self.assertEqual(json['extra2'], 'extra2')
 
 
 if __name__ == '__main__':
