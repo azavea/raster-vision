b749283beb23ffa5152aab30a26233fa04355afd
ahassan@azavea.com <adeel.hassan93@gmail.com>
2022-04-12 17:45:44 +0500

    update CC evaluation
    make it more similar to SS evaluation

--- rastervision_core/rastervision/core/evaluation/chip_classification_evaluation.py
+++ rastervision_core/rastervision/core/evaluation/chip_classification_evaluation.py
@@ -1,31 +1,27 @@
+from typing import TYPE_CHECKING
+
 import numpy as np
-from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 
 from rastervision.core.evaluation import (ClassificationEvaluation,
                                           ClassEvaluationItem)
+if TYPE_CHECKING:
+    from rastervision.core.data import (ChipClassificationLabels, ClassConfig)
 
 
 class ChipClassificationEvaluation(ClassificationEvaluation):
-    def __init__(self, class_config):
+    def __init__(self, class_config: 'ClassConfig'):
         super().__init__()
         self.class_config = class_config
 
-    def compute(self, ground_truth_labels, prediction_labels):
-        self.class_to_eval_item = ChipClassificationEvaluation.compute_eval_items(
-            ground_truth_labels, prediction_labels, self.class_config)
-
-        self.compute_avg()
-
-    @staticmethod
-    def compute_eval_items(gt_labels, pred_labels, class_config):
-        nb_classes = len(class_config.names)
-        class_to_eval_item = {}
+    def compute(self, gt_labels: 'ChipClassificationLabels',
+                pred_labels: 'ChipClassificationLabels') -> None:
+        self.reset()
+        self.class_to_eval_item = {}
 
         gt_class_ids = []
         pred_class_ids = []
-
-        gt_cells = gt_labels.get_cells()
-        for gt_cell in gt_cells:
+        for gt_cell in gt_labels.get_cells():
             gt_class_id = gt_labels.get_cell_class_id(gt_cell)
             pred_class_id = pred_labels.get_cell_class_id(gt_cell)
 
@@ -33,18 +29,15 @@ class ChipClassificationEvaluation(ClassificationEvaluation):
                 gt_class_ids.append(gt_class_id)
                 pred_class_ids.append(pred_class_id)
 
-        sklabels = np.arange(nb_classes)
-        precision, recall, f1, support = metrics.precision_recall_fscore_support(
-            gt_class_ids, pred_class_ids, labels=sklabels, warn_for=())
+        labels = np.arange(len(self.class_config))
+        self.conf_mat = confusion_matrix(
+            gt_class_ids, pred_class_ids, labels=labels)
 
-        for class_id, class_name in enumerate(class_config.names):
-            eval_item = ClassEvaluationItem(
-                float(precision[class_id]),
-                float(recall[class_id]),
-                float(f1[class_id]),
-                gt_count=float(support[class_id]),
+        for class_id, class_name in enumerate(self.class_config.names):
+            eval_item = ClassEvaluationItem.from_multiclass_conf_mat(
+                conf_mat=self.conf_mat,
                 class_id=class_id,
                 class_name=class_name)
-            class_to_eval_item[class_id] = eval_item
+            self.class_to_eval_item[class_id] = eval_item
 
-        return class_to_eval_item
+        self.compute_avg()
