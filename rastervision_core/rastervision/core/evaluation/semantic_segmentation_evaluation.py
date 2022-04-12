961995fe7ef81309cd7f788fe70b7fda2db4d71f
ahassan@azavea.com <adeel.hassan93@gmail.com>
2022-04-12 17:37:02 +0500

    update SS evaluation
    - do not omit the null class; treat it like any other class
    - make compute_vector() more similar to compute()
    - other refactoring for clarity

--- rastervision_core/rastervision/core/evaluation/semantic_segmentation_evaluation.py
+++ rastervision_core/rastervision/core/evaluation/semantic_segmentation_evaluation.py
@@ -1,4 +1,4 @@
-import math
+from typing import TYPE_CHECKING, Any, Iterable, List, Union
 import logging
 import json
 
@@ -8,10 +8,14 @@ import numpy as np
 from rastervision.core.evaluation import ClassEvaluationItem
 from rastervision.core.evaluation import ClassificationEvaluation
 
+if TYPE_CHECKING:
+    from rastervision.core.data import (
+        ClassConfig, SemanticSegmentationLabels, VectorOutputConfig)
+
 log = logging.getLogger(__name__)
 
 
-def is_geojson(data):
+def is_geojson(data: Any) -> bool:
     if isinstance(data, dict):
         return True
     else:
@@ -23,86 +27,56 @@ def is_geojson(data):
         return retval
 
 
-def get_class_eval_item(conf_mat, class_id, class_name, null_class_id):
-    if conf_mat.ravel().sum() == 0:
-        return ClassEvaluationItem(None, None, None, 0, 0, class_id,
-                                   class_name)
-
-    non_null_class_ids = list(range(conf_mat.shape[0]))
-    non_null_class_ids.remove(null_class_id)
-
-    true_pos = conf_mat[class_id, class_id]
-    false_pos = conf_mat[non_null_class_ids, class_id].sum() - true_pos
-    false_neg = conf_mat[class_id, :].sum() - true_pos
-    precision = float(true_pos) / (true_pos + false_pos)
-    recall = float(true_pos) / (true_pos + false_neg)
-    f1 = 2 * (precision * recall) / (precision + recall)
-    count_error = int(false_pos + false_neg)
-    gt_count = conf_mat[class_id, :].sum()
-
-    if math.isnan(precision):
-        precision = None
-    else:
-        precision = float(precision)
-    if math.isnan(recall):
-        recall = None
-    else:
-        recall = float(recall)
-    if math.isnan(f1):
-        f1 = None
-    else:
-        f1 = float(f1)
-
-    return ClassEvaluationItem(precision, recall, f1, count_error, gt_count,
-                               class_id, class_name, conf_mat[class_id, :])
-
-
 class SemanticSegmentationEvaluation(ClassificationEvaluation):
     """Evaluation for semantic segmentation."""
 
-    def __init__(self, class_config):
+    def __init__(self, class_config: 'ClassConfig'):
         super().__init__()
         self.class_config = class_config
 
-    def compute(self, gt_labels, pred_labels):
-        self.clear()
+    def compute(self, gt_labels: 'SemanticSegmentationLabels',
+                pred_labels: 'SemanticSegmentationLabels') -> None:
+        self.reset()
 
-        labels = np.arange(len(self.class_config.names))
-        conf_mat = np.zeros((len(labels), len(labels)))
+        # compute confusion matrix
+        num_classes = len(self.class_config)
+        labels = np.arange(num_classes)
+        self.conf_mat = np.zeros((num_classes, num_classes))
         for window in pred_labels.get_windows():
-            log.debug('Evaluating window: {}'.format(window))
+            log.debug(f'Evaluating window: {window}')
             gt_arr = gt_labels.get_label_arr(window).ravel()
             pred_arr = pred_labels.get_label_arr(window).ravel()
-            conf_mat += confusion_matrix(gt_arr, pred_arr, labels=labels)
+            self.conf_mat += confusion_matrix(gt_arr, pred_arr, labels=labels)
 
         for class_id, class_name in enumerate(self.class_config.names):
-            if class_id != self.class_config.get_null_class_id():
-                class_name = self.class_config.names[class_id]
-                self.class_to_eval_item[class_id] = get_class_eval_item(
-                    conf_mat, class_id, class_name,
-                    self.class_config.get_null_class_id())
+            eval_item = ClassEvaluationItem.from_multiclass_conf_mat(
+                conf_mat=self.conf_mat,
+                class_id=class_id,
+                class_name=class_name)
+            self.class_to_eval_item[class_id] = eval_item
 
         self.compute_avg()
 
-    def compute_vector(self, gt, pred, mode, class_id):
+    def compute_vector(self, gt: Union[str, dict],
+                       preds: Iterable[Union[str, dict]],
+                       vector_outputs: Iterable['VectorOutputConfig']) -> None:
         """Compute evaluation over vector predictions.
-            Args:
-                gt: Ground-truth GeoJSON.  Either a string (containing
-                    unparsed GeoJSON or a file name), or a dictionary
-                    containing parsed GeoJSON.
-                pred: GeoJSON for predictions.  Either a string
-                    (containing unparsed GeoJSON or a file name), or a
-                    dictionary containing parsed GeoJSON.
-                mode: A string containing either 'buildings' or
-                    'polygons'.
-                class_id: An integer containing the class id of
-                    interest.
+
+        Args:
+            gt (Union[str, dict]): Ground-truth GeoJSON. Either a string
+                (containing unparsed GeoJSON or a file name), or a dictionary
+                containing parsed GeoJSON.
+            preds (Iterable[Union[str, dict]]): Prediction GeoJSONs. Either a
+                string (containing unparsed GeoJSON or a file name), or a
+                dictionary containing parsed GeoJSON.
+            vector_outputs (Iterable[VectorOutputConfig]): VectorOutputConfig's
+                corresponding to each prediction in preds.
         """
         import mask_to_polygons.vectorification as vectorification
         import mask_to_polygons.processing.score as score
 
         # Ground truth as list of geometries
-        def get_geoms(x):
+        def get_geoms(x) -> List[dict]:
             if is_geojson(x):
                 _x = x
                 if 'features' in _x.keys():
@@ -119,33 +93,22 @@ class SemanticSegmentationEvaluation(ClassificationEvaluation):
             return geoms
 
         gt = get_geoms(gt)
-        pred = get_geoms(pred)
+        if len(gt) == 0:
+            return
 
-        if len(gt) > 0 and len(pred) > 0:
+        for pred, vo in zip(preds, vector_outputs):
+            pred = get_geoms(pred)
+            if len(pred) == 0:
+                continue
+            class_id = vo.class_id
             results = score.spacenet(pred, gt)
+            eval_item = ClassEvaluationItem(
+                class_id=class_id,
+                class_name=self.class_config.names[class_id],
+                tp=results['tp'],
+                fp=results['fp'],
+                fn=results['fn'],
+                mode=vo.get_mode())
+            self.class_to_eval_item[class_id] = eval_item
 
-            true_positives = results['tp']
-            false_positives = results['fp']
-            false_negatives = results['fn']
-            precision = float(true_positives) / (
-                true_positives + false_positives)
-            recall = float(true_positives) / (true_positives + false_negatives)
-            if precision + recall != 0:
-                f1 = 2 * (precision * recall) / (precision + recall)
-            else:
-                f1 = 0.0
-            count_error = int(false_positives + false_negatives)
-            gt_count = len(gt)
-            class_name = 'vector-{}-{}'.format(
-                mode, self.class_config.names[class_id])
-
-            evaluation_item = ClassEvaluationItem(precision, recall, f1,
-                                                  count_error, gt_count,
-                                                  class_id, class_name)
-
-            if hasattr(self, 'class_to_eval_item') and isinstance(
-                    self.class_to_eval_item, dict):
-                self.class_to_eval_item[class_id] = evaluation_item
-            else:
-                self.class_to_eval_item = {class_id: evaluation_item}
-            self.compute_avg()
+        self.compute_avg()
--- rastervision_core/rastervision/core/evaluation/semantic_segmentation_evaluator.py
+++ rastervision_core/rastervision/core/evaluation/semantic_segmentation_evaluator.py
@@ -1,17 +1,23 @@
+from typing import TYPE_CHECKING, Iterable, Iterator
 import logging
 
 from shapely.geometry import shape, mapping
 from shapely.strtree import STRtree
 
-from rastervision.core.data import ActivateMixin
+from rastervision.core.data import ActivateMixin, RasterizedSource
 from rastervision.core.data.vector_source import GeoJSONVectorSourceConfig
 from rastervision.core.evaluation import (ClassificationEvaluator,
                                           SemanticSegmentationEvaluation)
 
 log = logging.getLogger(__name__)
 
+if TYPE_CHECKING:
+    from rastervision.core.data import (Scene, ClassConfig,
+                                        SemanticSegmentationLabelSource,
+                                        SemanticSegmentationLabelStore)
 
-def filter_geojson_by_aoi(geojson, aoi_polygons):
+
+def filter_geojson_by_aoi(geojson: dict, aoi_polygons: list) -> dict:
     # Note that this ignores class_id but that's ok because each prediction GeoJSON file
     # covers a single class_id. But, this may change in the future.
     tree = STRtree([shape(f['geometry']) for f in geojson['features']])
@@ -34,23 +40,29 @@ class SemanticSegmentationEvaluator(ClassificationEvaluator):
     """Evaluates predictions for a set of scenes.
     """
 
-    def __init__(self, class_config, output_uri, vector_output_uri):
+    def __init__(self, class_config: 'ClassConfig', output_uri: str,
+                 vector_output_uri: str):
         super().__init__(class_config, output_uri)
         self.vector_output_uri = vector_output_uri
 
-    def create_evaluation(self):
+    def create_evaluation(self) -> SemanticSegmentationEvaluation:
         return SemanticSegmentationEvaluation(self.class_config)
 
-    def process(self, scenes, tmp_dir):
-        evaluation = self.create_evaluation()
-        vect_evaluation = self.create_evaluation()
+    def process(self, scenes: Iterable['Scene'], tmp_dir: str) -> None:
+        evaluation_global = self.create_evaluation()
+        vect_evaluation_global = self.create_evaluation()
         null_class_id = self.class_config.get_null_class_id()
 
         for scene in scenes:
-            log.info('Computing evaluation for scene {}...'.format(scene.id))
-            label_source = scene.ground_truth_label_source
-            label_store = scene.prediction_label_store
+            log.info(f'Computing evaluation for scene {scene.id}...')
+            label_source: 'SemanticSegmentationLabelSource' = (
+                scene.ground_truth_label_source)
+            label_store: 'SemanticSegmentationLabelStore' = (
+                scene.prediction_label_store)
             with ActivateMixin.compose(label_source, label_store):
+                # -----------
+                # raster eval
+                # -----------
                 ground_truth = label_source.get_labels()
                 predictions = label_store.get_labels()
 
@@ -60,38 +72,57 @@ class SemanticSegmentationEvaluator(ClassificationEvaluator):
                         scene.aoi_polygons, null_class_id)
                     predictions = predictions.filter_by_aoi(
                         scene.aoi_polygons, null_class_id)
-                scene_evaluation = self.create_evaluation()
-                scene_evaluation.compute(ground_truth, predictions)
-                evaluation.merge(scene_evaluation, scene_id=scene.id)
+                evaluation_scene = self.create_evaluation()
+                evaluation_scene.compute(ground_truth, predictions)
+                evaluation_global.merge(evaluation_scene, scene_id=scene.id)
 
-            if hasattr(label_source, 'raster_source') and hasattr(
-                    label_source.raster_source, 'vector_source') and hasattr(
-                        label_store, 'vector_output'):
-                gt_geojson = label_source.raster_source.vector_source.get_geojson(
-                )
-                for vo in label_store.vector_output:
-                    pred_geojson_uri = vo.uri
-                    mode = vo.get_mode()
-                    class_id = vo.class_id
-                    pred_geojson_source = GeoJSONVectorSourceConfig(
-                        uri=pred_geojson_uri, default_class_id=class_id).build(
-                            self.class_config,
-                            scene.raster_source.get_crs_transformer())
-                    pred_geojson = pred_geojson_source.get_geojson()
+                # -----------
+                # vector eval
+                # -----------
+                has_vector_gt = isinstance(label_source.raster_source,
+                                           RasterizedSource)
+                has_vector_preds = label_store.vector_outputs is not None
+                if not (has_vector_gt and has_vector_preds):
+                    continue
 
-                    if scene.aoi_polygons:
-                        gt_geojson = filter_geojson_by_aoi(
-                            gt_geojson, scene.aoi_polygons)
-                        pred_geojson = filter_geojson_by_aoi(
-                            pred_geojson, scene.aoi_polygons)
+                gt_geojson = (
+                    label_source.raster_source.vector_source.get_geojson())
+                if scene.aoi_polygons:
+                    gt_geojson = filter_geojson_by_aoi(gt_geojson,
+                                                       scene.aoi_polygons)
+                pred_geojsons = get_class_vector_preds(scene, label_store,
+                                                       self.class_config)
+                vect_evaluation_scene = self.create_evaluation()
+                vect_evaluation_scene.compute_vector(
+                    gt_geojson, pred_geojsons, label_store.vector_outputs)
+                vect_evaluation_global.merge(
+                    vect_evaluation_scene, scene_id=scene.id)
 
-                    vect_scene_evaluation = self.create_evaluation()
-                    vect_scene_evaluation.compute_vector(
-                        gt_geojson, pred_geojson, mode, class_id)
-                    vect_evaluation.merge(
-                        vect_scene_evaluation, scene_id=scene.id)
+        if not evaluation_global.is_empty():
+            evaluation_global.save(self.output_uri)
+        if not vect_evaluation_global.is_empty():
+            vect_evaluation_global.save(self.vector_output_uri)
 
-        if not evaluation.is_empty():
-            evaluation.save(self.output_uri)
-        if not vect_evaluation.is_empty():
-            vect_evaluation.save(self.vector_output_uri)
+
+def get_class_vector_preds(scene: 'Scene',
+                           label_store: 'SemanticSegmentationLabelStore',
+                           class_config: 'ClassConfig') -> Iterator[dict]:
+    """Returns a generator that yields pred geojsons from
+    label_store.vector_outputs."""
+    class_ids = [vo.class_id for vo in label_store.vector_outputs]
+    if len(set(class_ids)) < len(class_ids):
+        raise ValueError('SemanticSegmentationEvaluator expects there to be '
+                         'only one VectorOutputConfig per class.')
+    for vo in label_store.vector_outputs:
+        pred_geojson_uri = vo.uri
+        class_id = vo.class_id
+        pred_geojson_source_cfg = GeoJSONVectorSourceConfig(
+            uri=pred_geojson_uri, default_class_id=class_id)
+        pred_geojson_source = pred_geojson_source_cfg.build(
+            class_config, scene.raster_source.get_crs_transformer())
+        pred_geojson: dict = pred_geojson_source.get_geojson()
+
+        if scene.aoi_polygons:
+            pred_geojson = filter_geojson_by_aoi(pred_geojson,
+                                                 scene.aoi_polygons)
+        yield pred_geojson
