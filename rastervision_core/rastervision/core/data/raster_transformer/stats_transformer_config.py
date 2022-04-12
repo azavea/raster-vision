b7fcefbd2391a8bac234a40c8c13bffc99eeee32
ahassan@azavea.com <adeel.hassan93@gmail.com>
2022-04-12 16:05:57 +0500

    allow specifying scene group in StatsTransformerConfig

--- rastervision_core/rastervision/core/data/raster_transformer/stats_transformer_config.py
+++ rastervision_core/rastervision/core/data/raster_transformer/stats_transformer_config.py
@@ -1,25 +1,34 @@
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
 from os.path import join, basename
 
 from rastervision.pipeline.config import register_config, Field
-from rastervision.core.data.raster_transformer.raster_transformer_config import (  # noqa
-    RasterTransformerConfig)
-from rastervision.core.data.raster_transformer.stats_transformer import (  # noqa
-    StatsTransformer)
+from rastervision.core.data.raster_transformer import (RasterTransformerConfig,
+                                                       StatsTransformer)
 from rastervision.core.raster_stats import RasterStats
+
+if TYPE_CHECKING:
+    from rastervision.core.rv_pipeline import RVPipelineConfig
+    from rastervision.core.data import SceneConfig
 
 
 @register_config('stats_transformer')
 class StatsTransformerConfig(RasterTransformerConfig):
     stats_uri: Optional[str] = Field(
         None,
-        description=
-        ('The URI of the output of the StatsAnalyzer. If None, and this Config is '
-         'inside an RVPipeline, then this field will be auto-generated.'))
+        description='The URI of the output of the StatsAnalyzer. '
+        'If None, and this Config is inside an RVPipeline, '
+        'this field will be auto-generated.')
+    scene_group: str = Field(
+        'train_scenes',
+        description='Name of the group of scenes whose stats to use. Defaults'
+        'to "train_scenes".')
 
-    def update(self, pipeline=None, scene=None):
+    def update(self,
+               pipeline: Optional['RVPipelineConfig'] = None,
+               scene: Optional['SceneConfig'] = None) -> None:
         if pipeline is not None and self.stats_uri is None:
-            self.stats_uri = join(pipeline.analyze_uri, 'stats.json')
+            self.stats_uri = join(pipeline.analyze_uri, 'stats',
+                                  self.scene_group, 'stats.json')
 
     def build(self):
         return StatsTransformer(RasterStats.load(self.stats_uri))
