c1b46613cc0071ab570d26490dd04c0c9e0c6750
Adeel Hassan <adeel.hassan93@gmail.com>
2022-07-19 16:44:44 +0500

    Add evaluate_predictions() and evaluate_scene() to Evaluator interface

--- rastervision_core/rastervision/core/evaluation/evaluator.py
+++ rastervision_core/rastervision/core/evaluation/evaluator.py
@@ -1,9 +1,47 @@
+from typing import TYPE_CHECKING, Any, Iterable
 from abc import (ABC, abstractmethod)
+
+if TYPE_CHECKING:
+    from rastervision.core.data import Scene, Labels
 
 
 class Evaluator(ABC):
     """Evaluates predictions for a set of scenes."""
 
     @abstractmethod
-    def process(self, scenes):
+    def process(self, scenes: Iterable['Scene']) -> None:
+        """Evaluate all given scenes and save the evaluations.
+
+        Args:
+            scenes (Iterable[Scene]): Scenes to evaluate.
+        """
+        pass
+
+    @abstractmethod
+    def evaluate_scene(self, scene: 'Scene') -> Any:
+        """Evaluate predictions from a scene's labels store.
+
+        The predictions are evalated against ground truth labels from the
+        scene's label source.
+
+        Args:
+            scene (Scene): A scene with a label source and a label store.
+
+        Returns:
+            ClassificationEvaluation: The evaluation.
+        """
+        pass
+
+    @abstractmethod
+    def evaluate_predictions(self, ground_truth: 'Labels',
+                             predictions: 'Labels') -> Any:
+        """Evaluate predictions against ground truth.
+
+        Args:
+            ground_truth (Labels): Ground truth labels.
+            predictions (Labels): Predictions.
+
+        Returns:
+            Any: The evaluation.
+        """
         pass
