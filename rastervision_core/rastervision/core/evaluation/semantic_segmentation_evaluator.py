82d44d4ef12897514cfba8e55610b8429c7a80e9
Adeel Hassan <adeel.hassan93@gmail.com>
2022-07-19 16:51:00 +0500

    implement new Evaluator interface in subclasses

--- rastervision_core/rastervision/core/evaluation/semantic_segmentation_evaluator.py
+++ rastervision_core/rastervision/core/evaluation/semantic_segmentation_evaluator.py
@@ -1,8 +1,5 @@
-from typing import TYPE_CHECKING, Iterable, Iterator
+from typing import TYPE_CHECKING, Iterable, Iterator, Optional
 import logging
-
-from shapely.geometry import shape, mapping
-from shapely.strtree import STRtree
 
 from rastervision.core.data import ActivateMixin, RasterizedSource
 from rastervision.core.data.vector_source import GeoJSONVectorSourceConfig
@@ -12,96 +9,92 @@ from rastervision.core.evaluation import (ClassificationEvaluator,
 log = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
-    from rastervision.core.data import (Scene, ClassConfig,
-                                        SemanticSegmentationLabelSource,
-                                        SemanticSegmentationLabelStore)
-
-
-def filter_geojson_by_aoi(geojson: dict, aoi_polygons: list) -> dict:
-    # Note that this ignores class_id but that's ok because each prediction GeoJSON file
-    # covers a single class_id. But, this may change in the future.
-    tree = STRtree([shape(f['geometry']) for f in geojson['features']])
-    filtered_shapes = []
-    for aoi_poly in aoi_polygons:
-        shapes_in_aoi = tree.query(aoi_poly)
-        for s in shapes_in_aoi:
-            s_int = s.intersection(aoi_poly)
-            filtered_shapes.append(s_int)
-
-    features = [{
-        'type': 'feature',
-        'geometry': mapping(s)
-    } for s in filtered_shapes]
-
-    return {'type': 'FeatureCollection', 'features': features}
+    from rastervision.core.data import (
+        Scene, ClassConfig, SemanticSegmentationLabels,
+        SemanticSegmentationLabelSource, SemanticSegmentationLabelStore,
+        VectorOutputConfig)
 
 
 class SemanticSegmentationEvaluator(ClassificationEvaluator):
-    """Evaluates predictions for a set of scenes.
-    """
+    """Evaluates predictions for a set of scenes."""
 
-    def __init__(self, class_config: 'ClassConfig', output_uri: str,
-                 vector_output_uri: str):
+    def __init__(self,
+                 class_config: 'ClassConfig',
+                 output_uri: Optional[str] = None,
+                 vector_output_uri: Optional[str] = None):
         super().__init__(class_config, output_uri)
         self.vector_output_uri = vector_output_uri
 
     def create_evaluation(self) -> SemanticSegmentationEvaluation:
         return SemanticSegmentationEvaluation(self.class_config)
 
-    def process(self, scenes: Iterable['Scene'], tmp_dir: str) -> None:
-        evaluation_global = self.create_evaluation()
-        vect_evaluation_global = self.create_evaluation()
+    def process(self, scenes: Iterable['Scene'],
+                tmp_dir: Optional[str] = None) -> None:
+        """Override to also process vector predictions."""
+        super().process(scenes)
+
+        if self.vector_output_uri is not None:
+            evaluation_global = self.create_evaluation()
+            for scene in scenes:
+                log.info(
+                    f'Computing vector evaluation for scene {scene.id}...')
+                evaluation = self.evaluate_scene_vector(scene)
+                evaluation_global.merge(evaluation, scene_id=scene.id)
+            evaluation_global.save(self.vector_output_uri)
+
+    def evaluate_scene(self, scene: 'Scene') -> SemanticSegmentationEvaluation:
+        """Override to pass null_class_id to filter_by_aoi()."""
         null_class_id = self.class_config.get_null_class_id()
+        label_source: 'SemanticSegmentationLabelSource' = scene.label_source
+        label_store: 'SemanticSegmentationLabelStore' = scene.label_store
 
-        for scene in scenes:
-            log.info(f'Computing evaluation for scene {scene.id}...')
-            label_source: 'SemanticSegmentationLabelSource' = (
-                scene.ground_truth_label_source)
-            label_store: 'SemanticSegmentationLabelStore' = (
-                scene.prediction_label_store)
-            with ActivateMixin.compose(label_source, label_store):
-                # -----------
-                # raster eval
-                # -----------
-                ground_truth = label_source.get_labels()
-                predictions = label_store.get_labels()
+        with ActivateMixin.compose(label_source, label_store):
+            ground_truth = label_source.get_labels()
+            predictions = label_store.get_labels()
 
-                if scene.aoi_polygons:
-                    # Filter labels based on AOI.
-                    ground_truth = ground_truth.filter_by_aoi(
-                        scene.aoi_polygons, null_class_id)
-                    predictions = predictions.filter_by_aoi(
-                        scene.aoi_polygons, null_class_id)
-                evaluation_scene = self.create_evaluation()
-                evaluation_scene.compute(ground_truth, predictions)
-                evaluation_global.merge(evaluation_scene, scene_id=scene.id)
+        if scene.aoi_polygons:
+            ground_truth = ground_truth.filter_by_aoi(scene.aoi_polygons,
+                                                      null_class_id)
+            predictions = predictions.filter_by_aoi(scene.aoi_polygons,
+                                                    null_class_id)
+        evaluation = self.evaluate_predictions(ground_truth, predictions)
+        return evaluation
 
-                # -----------
-                # vector eval
-                # -----------
-                has_vector_gt = isinstance(label_source.raster_source,
-                                           RasterizedSource)
-                has_vector_preds = label_store.vector_outputs is not None
-                if not (has_vector_gt and has_vector_preds):
-                    continue
+    def evaluate_scene_vector(
+            self, scene: 'Scene') -> SemanticSegmentationEvaluation:
+        label_source: 'SemanticSegmentationLabelSource' = scene.label_source
+        label_store: 'SemanticSegmentationLabelStore' = scene.label_store
 
-                gt_geojson = (
-                    label_source.raster_source.vector_source.get_geojson())
-                if scene.aoi_polygons:
-                    gt_geojson = filter_geojson_by_aoi(gt_geojson,
-                                                       scene.aoi_polygons)
-                pred_geojsons = get_class_vector_preds(scene, label_store,
-                                                       self.class_config)
-                vect_evaluation_scene = self.create_evaluation()
-                vect_evaluation_scene.compute_vector(
-                    gt_geojson, pred_geojsons, label_store.vector_outputs)
-                vect_evaluation_global.merge(
-                    vect_evaluation_scene, scene_id=scene.id)
+        has_vector_gt = isinstance(label_source.raster_source,
+                                   RasterizedSource)
+        has_vector_preds = label_store.vector_outputs is not None
+        if not has_vector_gt:
+            raise ValueError('Cannot evaluate vector predictions: '
+                             'label source does not have vector ground truth.')
+        if not has_vector_preds:
+            raise ValueError('Cannot evaluate vector predictions: '
+                             'label store does not have vector outputs.')
 
-        if not evaluation_global.is_empty():
-            evaluation_global.save(self.output_uri)
-        if not vect_evaluation_global.is_empty():
-            vect_evaluation_global.save(self.vector_output_uri)
+        with ActivateMixin.compose(label_source, label_store):
+            gt_geojson = (
+                label_source.raster_source.vector_source.get_geojson())
+            if scene.aoi_polygons:
+                gt_geojson = filter_geojson_by_aoi(gt_geojson,
+                                                   scene.aoi_polygons)
+            pred_geojsons = get_class_vector_preds(scene, label_store,
+                                                   self.class_config)
+            evaluation = self.evaluate_vector_predictions(
+                gt_geojson, pred_geojsons, label_store.vector_outputs)
+        return evaluation
+
+    def evaluate_vector_predictions(
+            self, gt_geojson: 'SemanticSegmentationLabels',
+            pred_geojsons: Iterable[dict],
+            vector_outputs: Iterable['VectorOutputConfig']
+    ) -> SemanticSegmentationEvaluation:
+        evaluation = self.create_evaluation()
+        evaluation.compute_vector(gt_geojson, pred_geojsons, vector_outputs)
+        return evaluation
 
 
 def get_class_vector_preds(scene: 'Scene',
@@ -126,3 +119,26 @@ def get_class_vector_preds(scene: 'Scene',
             pred_geojson = filter_geojson_by_aoi(pred_geojson,
                                                  scene.aoi_polygons)
         yield pred_geojson
+
+
+def filter_geojson_by_aoi(geojson: dict, aoi_polygons: list) -> dict:
+    from shapely.geometry import shape, mapping
+    from shapely.strtree import STRtree
+
+    # Note that this ignores class_id but that's ok because each prediction
+    # GeoJSON file covers a single class_id. But, this may change in the
+    # future.
+    tree = STRtree([shape(f['geometry']) for f in geojson['features']])
+    filtered_shapes = []
+    for aoi_poly in aoi_polygons:
+        shapes_in_aoi = tree.query(aoi_poly)
+        for s in shapes_in_aoi:
+            s_int = s.intersection(aoi_poly)
+            filtered_shapes.append(s_int)
+
+    features = [{
+        'type': 'Feature',
+        'geometry': mapping(s)
+    } for s in filtered_shapes]
+
+    return {'type': 'FeatureCollection', 'features': features}
