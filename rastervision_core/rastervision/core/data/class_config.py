8b91be7d2832855484dead2ca0eb7af536e53d38
Adeel Hassan <adeel.hassan93@gmail.com>
2022-08-01 14:08:12 +0500

    refactor ClassConfig to use pydantic validators

--- rastervision_core/rastervision/core/data/class_config.py
+++ rastervision_core/rastervision/core/data/class_config.py
@@ -1,15 +1,21 @@
-from typing import List, Optional, Union
+from typing import List, Optional, Tuple, Union
 
 from rastervision.pipeline.config import (Config, register_config, ConfigError,
-                                          Field)
+                                          Field, validator)
 from rastervision.core.data.utils import color_to_triple
+
+DEFAULT_NULL_CLASS_NAME = 'null'
+DEFAULT_NULL_CLASS_COLOR = 'black'
 
 
 @register_config('class_config')
 class ClassConfig(Config):
     """Configures the class names that are being predicted."""
-    names: List[str] = Field(..., description='Names of classes.')
-    colors: Optional[List[Union[List, str]]] = Field(
+    names: List[str] = Field(
+        ...,
+        description='Names of classes. The i-th class in this list will have '
+        'class ID = i.')
+    colors: Optional[List[Union[str, Tuple]]] = Field(
         None,
         description=
         ('Colors used to visualize classes. Can be color strings accepted by '
@@ -17,44 +23,96 @@ class ClassConfig(Config):
          'for each class.'))
     null_class: Optional[str] = Field(
         None,
-        description=
-        ('Optional name of class in `names` to use as the null class. This is used in '
-         'semantic segmentation to represent the label for imagery pixels that are '
-         'NODATA or that are missing a label. If None, and this Config is part of a '
-         'SemanticSegmentationConfig, a null class will be added automatically.'
-         ))
+        description='Optional name of class in `names` to use as the null '
+        'class. This is used in semantic segmentation to represent the label '
+        'for imagery pixels that are NODATA or that are missing a label. '
+        f'If None and the class names include "{DEFAULT_NULL_CLASS_NAME}", '
+        'it will automatically be used as the null class. If None, and this '
+        'Config is part of a SemanticSegmentationConfig, a null class will be '
+        'added automatically.')
 
-    def get_class_id(self, name):
+    @validator('colors', always=True)
+    def validate_colors(cls, v: Optional[List[Union[str, Tuple]]],
+                        values: dict) -> Optional[List[Union[str, Tuple]]]:
+        """Compare length w/ names. Also auto-generate if not specified."""
+        class_names = values['names']
+        class_colors = v
+        if class_colors is None:
+            class_colors = [color_to_triple() for _ in class_names]
+        elif len(class_names) != len(class_colors):
+            raise ConfigError(f'len(class_names) ({len(class_names)}) != '
+                              f'len(class_colors) ({len(class_colors)})\n'
+                              f'class_names: {class_names}\n'
+                              f'class_colors: {class_colors}')
+        return class_colors
+
+    @validator('null_class', always=True)
+    def validate_null_class(cls, v: Optional[str],
+                            values: dict) -> Optional[str]:
+        """Check if in names. If 'null' in names, use it as null class."""
+        names = values['names']
+        if v is None:
+            if DEFAULT_NULL_CLASS_NAME in names:
+                v = DEFAULT_NULL_CLASS_NAME
+        else:
+            if v not in names:
+                raise ConfigError(
+                    f'The null_class, "{v}", must be in list of class names.')
+
+            # edge case
+            default_null_class_in_names = (DEFAULT_NULL_CLASS_NAME in names)
+            null_class_neq_default = (v != DEFAULT_NULL_CLASS_NAME)
+            if default_null_class_in_names and null_class_neq_default:
+                raise ConfigError(
+                    f'"{DEFAULT_NULL_CLASS_NAME}" is in names but the '
+                    f'specified null_class is something else ("{v}").')
+        return v
+
+    def get_class_id(self, name: str) -> int:
         return self.names.index(name)
 
-    def get_name(self, id):
+    def get_name(self, id: int) -> str:
         return self.names[id]
 
-    def get_null_class_id(self):
+    def get_null_class_id(self) -> int:
         if self.null_class is None:
             raise ValueError('null_class is not set')
         return self.get_class_id(self.null_class)
 
-    def get_color_to_class_id(self):
+    def get_color_to_class_id(self) -> dict:
         return dict([(self.colors[i], i) for i in range(len(self.colors))])
 
-    def ensure_null_class(self):
-        """Add a null class if one isn't set."""
-        if self.null_class is None:
-            self.null_class = 'null'
-            if self.null_class not in self.names:
-                self.names.append('null')
-                self.colors.append('black')
+    def ensure_null_class(self) -> None:
+        """Add a null class if one isn't set. This method is idempotent."""
+        if self.null_class is not None:
+            return
 
-    def update(self, pipeline=None):
-        if not self.colors:
-            self.colors = [color_to_triple() for _ in self.names]
+        null_class_name = DEFAULT_NULL_CLASS_NAME
+        null_class_color = DEFAULT_NULL_CLASS_COLOR
 
-    def validate_config(self):
-        if self.null_class is not None and self.null_class not in self.names:
-            raise ConfigError(
-                'The null_class: {} must be in list of class names.'.format(
-                    self.null_class))
+        # This might seeem redundant given the null class validator above, but
+        # is actually important. Sometimes there can be multiple ClassConfig
+        # instances that reference the same list objects for names and colors
+        # (not clear why this happens). This means that
+        # each ensure_null_class() call will add to names and colors in each
+        # copy of ClassConfig but only set its own null_class, which makes this
+        # method() non-idempotent.
+        if null_class_name in self.names:
+            self.null_class = null_class_name
+            return
 
-    def __len__(self):
+        # use random color if default color is already taken
+        null_class_color_triple = color_to_triple(null_class_color)
+        all_color_triples = [
+            color_to_triple(c) if isinstance(c, str) else c
+            for c in self.colors
+        ]
+        if null_class_color_triple in all_color_triples:
+            null_class_color = color_to_triple()
+
+        self.names.append(null_class_name)
+        self.colors.append(null_class_color)
+        self.null_class = null_class_name
+
+    def __len__(self) -> int:
         return len(self.names)
