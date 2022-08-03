cd6956fad398d121d041404ffc99b00cb7099f1c
Adeel Hassan <adeel.hassan93@gmail.com>
2022-08-03 14:26:29 +0500

    remove erroneously defined register_plugin()

--- rastervision_core/rastervision/core/data/raster_source/__init__.py
+++ rastervision_core/rastervision/core/data/raster_source/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa
-
 from rastervision.core.data.raster_source.raster_source import *
 from rastervision.core.data.raster_source.raster_source_config import *
 from rastervision.core.data.raster_source.rasterio_source import *
@@ -8,7 +7,3 @@ from rastervision.core.data.raster_source.rasterized_source import *
 from rastervision.core.data.raster_source.rasterized_source_config import *
 from rastervision.core.data.raster_source.multi_raster_source import *
 from rastervision.core.data.raster_source.multi_raster_source_config import *
-
-
-def register_plugin(registry):
-    registry.set_plugin_version('rastervision.core', 1)
