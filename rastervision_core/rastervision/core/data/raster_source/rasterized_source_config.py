653d5983968a126f293f5277117c5accf2ffbea4
Adeel Hassan <adeel.hassan93@gmail.com>
2022-08-24 14:01:36 +0500

    refactor rasterized source
    - remove RasterizerConfig from RasterizedSource
    - add raster_transformers argument
    - remove crs_transformer argument
    - improve docs

--- rastervision_core/rastervision/core/data/raster_source/rasterized_source_config.py
+++ rastervision_core/rastervision/core/data/raster_source/rasterized_source_config.py
@@ -7,16 +7,14 @@ from rastervision.pipeline.config import register_config, Config, Field
 class RasterizerConfig(Config):
     background_class_id: int = Field(
         ...,
-        description=
-        ('The class_id to use for any background pixels, ie. pixels not covered by a '
-         'polygon.'))
+        description='The class_id to use for any background pixels, i.e. '
+        'pixels not covered by a polygon.')
     all_touched: bool = Field(
         False,
-        description=(
-            'If True, all pixels touched by geometries will be burned in. '
-            'If false, only pixels whose center is within the polygon or '
-            'that are selected by Bresenham’s line algorithm will be '
-            'burned in. (See rasterio.features.rasterize).'))
+        description='If True, all pixels touched by geometries will be burned '
+        'in. If false, only pixels whose center is within the polygon or that '
+        'are selected by Bresenham’s line algorithm will be burned in. '
+        '(See rasterio.features.rasterize for more details).')
 
 
 @register_config('rasterized_source')
@@ -27,5 +25,9 @@ class RasterizedSourceConfig(Config):
     def build(self, class_config, crs_transformer, extent):
         vector_source = self.vector_source.build(class_config, crs_transformer)
 
-        return RasterizedSource(vector_source, self.rasterizer_config, extent,
-                                crs_transformer)
+        return RasterizedSource(
+            vector_source=vector_source,
+            background_class_id=self.rasterizer_config.background_class_id,
+            extent=extent,
+            all_touched=self.rasterizer_config.all_touched)
+            all_touched=self.all_touched)
