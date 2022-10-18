3c5c7a699f1df37d9c9369952130c67c3246554b
Adeel Hassan <adeel.hassan93@gmail.com>
2022-10-18 13:19:56 +0500

    remove plugin aliases

--- rastervision_aws_batch/rastervision/aws_batch/__init__.py
+++ rastervision_aws_batch/rastervision/aws_batch/__init__.py
@@ -5,8 +5,6 @@ def register_plugin(registry):
     from rastervision.aws_batch.aws_batch_runner import (AWS_BATCH,
                                                          AWSBatchRunner)
     registry.set_plugin_version('rastervision.aws_batch', 0)
-    registry.set_plugin_aliases('rastervision.aws_batch',
-                                ['rastervision2.aws_batch'])
     registry.add_runner(AWS_BATCH, AWSBatchRunner)
     registry.add_rv_config_schema(AWS_BATCH, [
         'gpu_job_queue', 'gpu_job_def', 'cpu_job_queue', 'cpu_job_def',
