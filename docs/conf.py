8b2d8677c87285a68d0dadf1743a37072895bee5
Adeel Hassan <ahassan@azavea.com>
2022-10-24 17:08:09 +0500

    fix syntax highlighting issue on RTD in nb code cells

--- docs/conf.py
+++ docs/conf.py
@@ -173,7 +173,9 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
+exclude_patterns = [
+    '_build', 'Thumbs.db', '.DS_Store', 'README.md', '**.ipynb_checkpoints'
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 # pygments_style = 'sphinx'
