83e9a0686251d8281dd76f309fe356c0da0ae8fd
Adeel Hassan <ahassan@azavea.com>
2022-11-15 14:58:45 +0500

    minor fixes

--- rastervision_core/rastervision/core/rv_pipeline/utils.py
+++ rastervision_core/rastervision/core/rv_pipeline/utils.py
@@ -1,12 +1,24 @@
-# from typing import
 import numpy as np
 
 
 def nodata_below_threshold(chip: np.ndarray,
                            threshold: float,
                            nodata_val: int = 0) -> bool:
-    """ Check if proportion of nodata pixels is below the threshold. """
+    """Check if proportion of nodata pixels is below the threshold.
+
+    Args:
+        chip (np.ndarray): Raster as (H, W) or (H, W, C) numpy array.
+        threshold (float): Threshold to check the fraction of NODATA pixels
+            against.
+        nodata_val (int, optional): Value that represents NODATA pixels.
+            Defaults to 0.
+
+    Returns:
+        bool: Whether the fraction of NODATA pixels is below the given
+            threshold.
+    """
     if len(chip.shape) == 3:
+        # (H, W, C) --> (H, W)
         chip = chip.sum(axis=-1)
-    nodata_prop = (chip == 0).mean()
+    nodata_prop = (chip == nodata_val).mean()
     return nodata_prop < threshold
