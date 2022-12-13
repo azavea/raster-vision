382dbedf374add05caae12421205bf1a8c017781
Adeel Hassan <ahassan@azavea.com>
2022-12-13 18:21:13 +0500

    remove unused rastervision.core.data.dataset.Dataset

--- rastervision_core/rastervision/core/data/dataset.py
+++
@@ -1,7 +0,0 @@
-class Dataset:
-    """Comprises the scenes for train, valid, and test splits."""
-
-    def __init__(self, train_scenes=[], validation_scenes=[], test_scenes=[]):
-        self.train_scenes = train_scenes
-        self.validation_scenes = validation_scenes
-        self.test_scenes = test_scenes
