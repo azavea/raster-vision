57515c6dc955f0f36e6440a8e61d0f50a71c13ea
Adeel Hassan <ahassan@element84.com>
2023-09-14 13:50:04 -0400

    relocate save_img() from core.utils.misc to core.data.utils.misc

--- rastervision_core/rastervision/core/utils/misc.py
+++ rastervision_core/rastervision/core/utils/misc.py
@@ -1,13 +1,3 @@
-from typing import TYPE_CHECKING
 from pydantic import confloat
 
-from skimage.io import imsave
-
-if TYPE_CHECKING:
-    import numpy as np
-
 Proportion = confloat(ge=0, le=1)
-
-
-def save_img(im_array: 'np.ndarray', output_path: str):
-    imsave(output_path, im_array)
