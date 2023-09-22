4f454cf48599d5bbcbd5571bfc4a31072be5c739
Adeel Hassan <ahassan@element84.com>
2023-09-22 14:32:31 -0400

    add scikit-image as an explicit dependency and remove imageio

--- rastervision_core/rastervision/core/utils/misc.py
+++ rastervision_core/rastervision/core/utils/misc.py
@@ -1,12 +1,13 @@
+from typing import TYPE_CHECKING
 from pydantic import confloat
 
-import imageio
-import logging
+from skimage.io import imsave
+
+if TYPE_CHECKING:
+    import numpy as np
 
 Proportion = confloat(ge=0, le=1)
 
-log = logging.getLogger(__name__)
 
-
-def save_img(im_array, output_path):
-    imageio.imwrite(output_path, im_array)
+def save_img(im_array: 'np.ndarray', output_path: str):
+    imsave(output_path, im_array)
