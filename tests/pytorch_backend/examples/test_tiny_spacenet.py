6a3fab79da20c3ebd373ccf06c9a78ac5d34646d
Adeel Hassan <ahassan@element84.com>
2023-10-18 14:29:33 -0400

    run the predict command as part of tiny_spacenet unit test

--- tests/pytorch_backend/examples/test_tiny_spacenet.py
+++ tests/pytorch_backend/examples/test_tiny_spacenet.py
@@ -4,6 +4,10 @@ import shutil
 from click.testing import CliRunner
 
 from rastervision.pipeline.cli import main
+from rastervision.pipeline.file_system.utils import get_tmp_dir
+from rastervision.core.cli import predict
+
+from tests import data_file_path
 
 
 class TestTinySpacenet(unittest.TestCase):
@@ -14,6 +18,16 @@ class TestTinySpacenet(unittest.TestCase):
             'run', 'inprocess',
             'rastervision.pytorch_backend.examples.tiny_spacenet'
         ])
+        self.assertEqual(result.exit_code, 0)
+
+        # test predict command
+        bundle_path = '/opt/data/output/tiny_spacenet/bundle/model-bundle.zip'
+        img_path = data_file_path('small-rgb-tile.tif')
+        with get_tmp_dir() as tmp_dir:
+            result = runner.invoke(predict, [
+                bundle_path, img_path, tmp_dir, '--channel-order', '0', '1',
+                '2'
+            ])
         self.assertEqual(result.exit_code, 0)
 
 
