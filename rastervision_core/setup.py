e104c812130e2d5a9b76c9c35a3bb74a706d9210
Adeel Hassan <AdeelH@users.noreply.github.com>
2024-04-11 16:02:44 -0400

    bump version to 0.30.1-dev (#2110)
    Co-authored-by: Adeel Hassan <ahassan@element84.com>

--- rastervision_core/setup.py
+++ rastervision_core/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_namespace_packages
 import re
 
 name = 'rastervision_core'
-version = '0.30.0'
+version = '0.30.1-dev'
 description = 'A rastervision plugin that adds geospatial machine learning pipelines'
 requirement_constraints = {}
 
