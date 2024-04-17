58e7db42abffedb96409db5813530fc00424bf99
Adeel Hassan <ahassan@element84.com>
2024-04-17 16:42:04 -0400

    let .get_chip_by_map_window() take all .get_chip() args

--- rastervision_core/rastervision/core/data/raster_source/raster_source.py
+++ rastervision_core/rastervision/core/data/raster_source/raster_source.py
@@ -147,24 +147,20 @@ class RasterSource(ABC):
 
         return chip
 
-    def get_chip_by_map_window(
-            self,
-            window_map_coords: 'Box',
-            out_shape: Optional[Tuple[int, int]] = None) -> 'np.ndarray':
-        """Same as get_chip(), but input is a window in map coords. """
+    def get_chip_by_map_window(self, window_map_coords: 'Box', *args,
+                               **kwargs) -> 'np.ndarray':
+        """Same as get_chip(), but input is a window in map coords."""
         window_pixel_coords = self.crs_transformer.map_to_pixel(
             window_map_coords, bbox=self.bbox).normalize()
-        chip = self.get_chip(window_pixel_coords, out_shape=out_shape)
+        chip = self.get_chip(window_pixel_coords, *args, **kwargs)
         return chip
 
-    def _get_chip_by_map_window(
-            self,
-            window_map_coords: 'Box',
-            out_shape: Optional[Tuple[int, int]] = None) -> 'np.ndarray':
-        """Same as _get_chip(), but input is a window in map coords. """
+    def _get_chip_by_map_window(self, window_map_coords: 'Box', *args,
+                                **kwargs) -> 'np.ndarray':
+        """Same as _get_chip(), but input is a window in map coords."""
         window_pixel_coords = self.crs_transformer.map_to_pixel(
             window_map_coords, bbox=self.bbox)
-        chip = self._get_chip(window_pixel_coords, out_shape=out_shape)
+        chip = self._get_chip(window_pixel_coords, *args, **kwargs)
         return chip
 
     def get_raw_chip(self,
